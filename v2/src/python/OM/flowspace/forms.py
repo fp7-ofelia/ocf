c323b5a2c0627d2ba74ec708c1587d31b53e78f6
Peyman Kazemian <peymankazemian@dn0a2069de.sunet>
2010-04-29 21:05:49 -0700

    old om removed

--- v2/src/python/OM/flowspace/forms.py
+++
@@ -1,8 +0,0 @@
-# forms to be used with flowspace app
-from django import forms
-from OM.flowspace.models import OptedInFlowSpace, AdminFlowSpace, RequestedAdminFlowSpace, RequestedUserFlowSpace
-
-class OptedInFlowSpaceForm(forms.ModelForm):
-    class Meta:
-        model = OptedInFlowSpace
-        exclude = ('user',)
