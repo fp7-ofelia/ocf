96bb1f0cbaadf930df7633f6009528d773db460b
Jad Naous <jnaous@stanford.edu>
2010-05-15 15:01:56 -0700

    Fix DB location

--- v2/src/python/clearinghouse/settings.py
+++ v2/src/python/clearinghouse/settings.py
@@ -16,7 +16,7 @@ STATIC_DOC_ROOT = join(SRC_DIR, 'static')
 MANAGERS = ADMINS
 
 DATABASE_ENGINE = 'sqlite3'           # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-DATABASE_NAME = join("/tmp", 'db/clearinghouse.db')         # Or path to database file if using sqlite3.
+DATABASE_NAME = join(SRC_DIR, 'db/clearinghouse.db')         # Or path to database file if using sqlite3.
 DATABASE_USER = ''             # Not used with sqlite3.
 DATABASE_PASSWORD = ''         # Not used with sqlite3.
 DATABASE_HOST = ''             # Set to empty string for localhost. Not used with sqlite3.
