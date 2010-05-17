782864110442d9338a15ec75782a4fe2b138f52a
Jad Naous <jnaous@stanford.edu>
2010-05-17 15:12:24 -0700

    added paths to test_settings for the OM

--- tests/src/python/tests/om/test_settings.py
+++ tests/src/python/tests/om/test_settings.py
@@ -5,9 +5,14 @@ Created on May 15, 2010
 '''
 from os.path import join, dirname, basename
 import socket
+import sys
 
 EGENI_DIR = join(dirname(__file__), "../../../../../")
 OM_PROJECT_DIR = join(EGENI_DIR, "om/src/python/optin_manager")
+CH_PROJECT_DIR = join(EGENI_DIR, "v2/src/python/clearinghouse")
+
+sys.path.append(join(OM_PROJECT_DIR, ".."))
+sys.path.append(join(CH_PROJECT_DIR, ".."))
 
 NUM_DUMMY_FVS = 1
 
