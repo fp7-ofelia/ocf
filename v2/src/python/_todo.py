cd596436a5be6c0ab1040da7943060f790f4b165
Jad Naous <jnaous@stanford.edu>
2010-05-19 05:05:41 -0700

    update todos

--- v2/src/python/_todo.py
+++ v2/src/python/_todo.py
@@ -3,9 +3,9 @@ Created on May 14, 2010
 
 @author: jnaous
 '''
-# TODO: write up the OM harness
 # TODO: Add permissions to XMLRPC calls
 # TODO: Limit the scope of callable XMLRPC calls depending on the url
-# TODO: Check the security of calls to the GAPI, use x509 certs?
+# TODO: Check the security of calls to the Django GAPI, use x509 certs?
 # TODO: Add tests for CH ListResources with slice_urn specified
 # TODO: Add tests for CH ListResources with geni_available=False
+# TODO: Add tests for adding an Aggregate to the CH
