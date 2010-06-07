79d6a9288374608e5f7bcd0c3835d05683e4514b
unknown <jnaous@.(none)>
2010-06-06 23:18:23 -0700

    added namespace parameter to rpc4django decorator

--- src/python/expedient/common/rpc4django/tests/testmod/__init__.py
+++ src/python/expedient/common/rpc4django/tests/testmod/__init__.py
@@ -5,6 +5,6 @@ import testsubmod
 def subtract(a, b):
     return a - b
 
-@rpcmethod(signature=['int', 'int', 'int'])
+@rpcmethod(signature=['int', 'int', 'int'], url_ns="some_ns")
 def product(a, b):
     return a * b
--- src/python/expedient/common/rpc4django/tests/testmod/testsubmod/__init__.py
+++ src/python/expedient/common/rpc4django/tests/testmod/testsubmod/__init__.py
@@ -3,3 +3,7 @@ from rpc4django import rpcmethod
 @rpcmethod(signature=['int', 'int', 'int'])
 def power(a, b):
     return a ** b
+
+@rpcmethod(signature=['int', 'int'], url_ns="some_ns")
+def power2(a):
+    return 2 ** a
