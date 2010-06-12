150be287afa47d7f8926a55d5bc506de4be899e3
Jad Naous <jnaous@beirut.stanford.edu>
2010-06-11 19:12:04 -0700

    updated makefile for distribution and distributed documentation for installation

--- dist/install.sh
+++ dist/install.sh
@@ -15,12 +15,12 @@ OPTIN_MANAGER="openflow/optin_manager"
 
 # install prereqs
 zypper addrepo -f http://download.opensuse.org/repositories/devel:/languages:/python/openSUSE_11.2 python
-zypper addrepo zypper addrepo -f http://download.opensuse.org/repositories/openSUSE:/11.2:/Contrib/standard/ contrib
+zypper addrepo -f http://download.opensuse.org/repositories/openSUSE:/11.2:/Contrib/standard/ contrib
 zypper install -l python-setuptools python-django python-decorator python-django-autoslug python-m2crypto python-curl python-imaging python-django-extensions python-dateutil libxmlsec1-1 libxmlsec1-openssl1 python-paramiko python-crypto
 easy_install -Z django-registration python-xmlsec1
 zypper install -l -f --force-resolution libxmlsec1-openssl-devel
 
-cp -R expedient-$VERSION $BASE
+cp -R ../expedient-$VERSION $BASE
 
 chown wwwrun:www -R $BASE
 chmod g+r -R $BASE
@@ -38,7 +38,7 @@ zypper install apache2 apache2-mod_wsgi apache2-mod_macro apache2-itk
 # Add configuration
 cp -s $BASE/src/config/$COMMON/apache/vhost-macros.conf /etc/apache2/conf.d/
 cp -s $BASE/src/config/$CLEARINGHOUSE/apache/vhost-clearinghouse.conf /etc/apache2/vhosts.d/
-cp -s $BASE/src/config/$OPTIN_MANAGER/apache/vhost-optinmgr.conf /etc/apache2/conf.d/vhosts.d/
+cp -s $BASE/src/config/$OPTIN_MANAGER/apache/vhost-optinmgr.conf /etc/apache2/conf.d/
 
 # Create Apache log directories
 mkdir -p /var/log/apache2/$CLEARINGHOUSE
