f4072d0e7c9d13c88866a93a4f623b63dba0082a
Peyman Kazemian <peyman@linux-wrw3.site>
2010-06-15 17:55:36 -0700

    automatically assign full fs to superuser

--- src/python/openflow/optin_manager/users/views.py
+++ src/python/openflow/optin_manager/users/views.py
@@ -4,6 +4,8 @@ from django.contrib.auth.decorators import login_required
 from django.http import *
 from django.http import HttpResponse
 from django.views.generic import simple
+from openflow.optin_manager.opts.models import AdminFlowSpace
+from openflow.optin_manager.users.models import Priority
 
 def index(request):
     if request.user.is_authenticated():
@@ -13,6 +15,18 @@ def index(request):
 
 @login_required
 def dashboard(request):
+    # if this the first time that admin logs in, give him all FS:
+    if (request.user.is_superuser):
+        p = UserProfile.get_or_create_profile(request.user)
+        p.is_net_admin = True
+        p.priority = Priority.Aggregate_Admin
+        p.supervisor = request.user
+        p.save()
+
+        list = AdminFlowSpace.objects.filter(user=request.user)
+        if len(list)==0:
+            AdminFlowSpace.objects.create(user=request.user)
+    
     profile = UserProfile.get_or_create_profile(request.user)
     username = profile.user.username
     return simple.direct_to_template(request, 
