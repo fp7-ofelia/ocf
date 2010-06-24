ccd7caf7b231abeaec67708e4036c92984884e7e
Jad Naous <jnaous@beirut.stanford.edu>
2010-06-23 22:54:45 -0700

    fixed bugs in install scripts

--- dist/install_clean.sh
+++ dist/install_clean.sh
@@ -2,17 +2,13 @@
 
 source ./install_settings
 
+sed -i "{s/SITE_DOMAIN.*/SITE_DOMAIN = \"$CH_FQDN\"/}" $BASE/src/python/$CLEARINGHOUSE/deployment_settings.py
+sed -i "{s/SITE_DOMAIN.*/SITE_DOMAIN = \"$OM_FQDN\"/}" $BASE/src/python/$OPTIN_MANAGER/deployment_settings.py
+
 # syncdb
 python $BASE/src/python/$OPTIN_MANAGER/manage.py syncdb
-python $BASE/src/python/$OPTIN_MANAGER/manage.py flush
 python $BASE/src/python/$CLEARINGHOUSE/manage.py syncdb
-python $BASE/src/python/$CLEARINGHOUSE/manage.py flush
-
-
-# Set the hostname
-FQDN=`hostname -f`
-sed -i "{s/SITE_DOMAIN.*/SITE_DOMAIN = \"$FQDN\"/}" $BASE/src/python/$CLEARINGHOUSE/deployment_settings.py
-sed -i "{s/SITE_DOMAIN.*/SITE_DOMAIN = \"$FQDN\"/}" $BASE/src/python/$OPTIN_MANAGER/deployment_settings.py
 
 # restart apache
 /etc/init.d/apache2 restart
+
