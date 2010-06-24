87d3b383826098c1fe48023b155f9f15ec1bbc05
Jad Naous <jnaous@beirut.stanford.edu>
2010-06-23 23:21:55 -0700

    more bug fixes

--- dist/install.sh
+++ dist/install.sh
@@ -11,24 +11,18 @@ source ./install_settings
 # install prereqs
 zypper addrepo -f http://download.opensuse.org/repositories/devel:/languages:/python/openSUSE_11.2 python
 zypper addrepo -f http://download.opensuse.org/repositories/openSUSE:/11.2:/Contrib/standard/ contrib
-zypper install -l python-setuptools python-django python-decorator python-django-autoslug python-m2crypto python-curl python-imaging python-django-extensions python-dateutil libxml libxml2 gcc libxmlsec1-1 libxmlsec1-openssl1 python-paramiko python-crypto python-django-renderform python-webob sqlite3 python-openssl gcc
+zypper addrepo -f http://download.opensuse.org/repositories/Apache:/Modules/openSUSE_11.2/ Apache:Modules
+
+echo '----- Ignore errors next and force the install option 2:'
+zypper install -l python-setuptools python-django python-decorator python-django-autoslug python-m2crypto python-curl python-imaging python-django-extensions python-dateutil libxml libxml2 gcc libxmlsec1-1 libxmlsec1-openssl1 python-paramiko python-crypto python-django-renderform python-webob sqlite3 python-openssl gcc apache2 apache2-mod_wsgi apache2-mod_macro apache2-itk libxmlsec1-openssl-devel
 
 easy_install -Z django-registration pyquery
-
-echo **** Ignore errors next and force the install option 2:
-zypper install -l -f --force-resolution libxmlsec1-openssl-devel
 
 cp -R ../expedient-$VERSION $BASE
 
 chown -R wwwrun:www $BASE
 chmod -R g+r $BASE
 chmod -R g+w $BASE/db
-chgrp www /etc/apache2/ssl.crt
-chmod g+rw /etc/apache2/ssl.crt
-
-# Install Apache prereqs
-zypper addrepo -f http://download.opensuse.org/repositories/Apache:/Modules/openSUSE_11.2/ Apache:Modules
-zypper install apache2 apache2-mod_wsgi apache2-mod_macro apache2-itk
 
 # Add configuration
 cp -s $BASE/src/config/$COMMON/apache/vhost-macros.conf /etc/apache2/conf.d/
@@ -49,6 +43,9 @@ a2enflag SSL
 
 # Generate server certificates
 /usr/bin/gensslcert
+
+chgrp www /etc/apache2/ssl.crt
+chmod g+rw /etc/apache2/ssl.crt
 
 # restart apache
 /etc/init.d/apache2 restart
