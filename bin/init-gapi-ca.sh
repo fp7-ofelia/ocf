50dffe524c57372f80ace5e72fb4ff26d8c211cf
Jad Naous <jnaous@beirut.stanford.edu>
2010-07-01 04:29:50 -0700

    fix ssl dir

--- bin/init-gapi-ca.sh
+++ bin/init-gapi-ca.sh
@@ -4,4 +4,4 @@ source expedient-settings
 
 # initialize the CA used for the GENI API
 mkdir -p $GAPI_SSL_DIR
-python /home/expedient/expedient/src/python/gcf/init-ca.py -d ssl --am --ch --ca --exp
+python /home/expedient/expedient/src/python/gcf/init-ca.py -d $GAPI_SSL_DIR --am --ch --ca --exp
