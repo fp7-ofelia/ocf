af1d5a3f42d42c93961cd9c7368251b7f2f63dcf
Jad Naous <jnaous@beirut.stanford.edu>
2010-07-01 03:16:03 -0700

    fix issue with titles?

--- bin/run_tests.sh
+++ bin/run_tests.sh
@@ -3,16 +3,16 @@
 source expedient-settings
 
 echo ------------------------------------------
-echo -         Running GAPI Tests             -
+echo "-         Running GAPI Tests             -"
 echo ------------------------------------------
 python $EXPEDIENT/src/python/openflow/tests/gapi/gapi.py
 
 echo ------------------------------------------
-echo -          Running OM Tests              -
+echo "-          Running OM Tests              -"
 echo ------------------------------------------
 python $EXPEDIENT/src/python/openflow/tests/om/om.py
 
 echo ------------------------------------------
-echo -     Running Integration Tests          -
+echo "-     Running Integration Tests          -"
 echo ------------------------------------------
 python $EXPEDIENT/src/python/openflow/tests/full/fulltests.py
