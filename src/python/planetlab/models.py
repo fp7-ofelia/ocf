45052f0407139a3914a57d26f55feed31094bbb5
Jad Naous <jnaous@stanford.edu>
2010-07-04 13:13:19 -0700

    interim

--- src/python/planetlab/models.py
+++ src/python/planetlab/models.py
@@ -6,14 +6,31 @@ Created on Jul 4, 2010
 from django.db import models
 from expedient.clearinghouse.resources.models import Resource
 
-class PlanetLabInterface(Resource):
+class PlanetLabInterface(models.Model):
     '''
     A PlanetLab Interface (IfSpec)
     '''
-    
+    name = models.CharField(max_length=100)
     addr = models.CharField("Address", max_length=200)
     type = models.CharField(max_length=60)
-    init_params = models.CharField()
+    init_params = models.TextField()
+    min_rate = models.IntegerField("Minimum Throughput")
+    max_rate = models.IntegerField("Maximum Throughput")
+    ip_spoof = models.BooleanField("Spoof IP Address?")
+    
+    link = models.ForeignKey("PlanetLabLink", related_name="endpoints")
+    
+class PlanetLabLink(models.Model):
+    """
+    A PlanetLab Rspec LinkSpec.
+    """
+    type = models.CharField(max_length=60)
+    init_params = models.TextField()
+    bw = models.IntegerField("Bandwidth")
+    min_alloc = models.IntegerField("Minimum allocation")
+    max_alloc = models.IntegerField("Maximum allocation")
+    start_time = models.DateTimeField()
+    end_time = models.DateTimeField()
 
 class PlanetLabNode(Resource):
     '''
