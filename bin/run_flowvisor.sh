c63f57874408eb37e32debc8583e7cec1cc8b58a
Rob Sherwood <rob.sherwood@stanford.edu>
2010-07-08 01:49:56 -0700

    added '-L' option to screen for auto logging
    in run_flowvisor.sh ; might want to add it to other places
    logs to ~/screenlog.0

--- bin/run_flowvisor.sh
+++ bin/run_flowvisor.sh
@@ -4,5 +4,5 @@ source expedient-settings
 
 echo Starting the FlowVisor in a detached screen named flowvisor...
 # Run the FV
-screen -S "flowvisor" -dm $FLOWVISOR/scripts/flowvisor.sh $FLOWVISOR/default-config.xml
+screen -S "flowvisor" -L -dm $FLOWVISOR/scripts/flowvisor.sh $FLOWVISOR/default-config.xml
 echo To access the screen, use the command: screen -rd flowvisor
