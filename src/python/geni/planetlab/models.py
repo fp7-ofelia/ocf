94813efdeda87c55b1df62431992f5652d648fe8
Jad Naous <jnaous@beirut.stanford.edu>
2010-07-12 15:04:19 -0700

    take out slice info, add rspec parsing/creating

--- src/python/geni/planetlab/models.py
+++ src/python/geni/planetlab/models.py
@@ -5,17 +5,10 @@ Created on Jul 4, 2010
 '''
 from django.db import models
 from expedient.clearinghouse.resources.models import Resource, Sliver
-from geni.models import GENIAggergate, GENIAggregateSliceInfo
+from geni.models import GENIAggregate, GENISliceInfo
 from xml.etree import cElementTree as et
 import calendar
-
-class PlanetLabSliceInfo(GENIAggregateSliceInfo):
-    """
-    Subclasses L{GENIAggregateSliceInfo} to specify additional attributes
-    for PlanetLab RSpecs
-    """
-    start_time = models.DateTimeField()
-    end_time = models.DateTimeField()
+from exceptions import RSpecParsingException, NameConflictException
 
 def get_start_duration(start_time=None, end_time=None):
     """
@@ -44,10 +37,71 @@ def get_start_duration(start_time=None, end_time=None):
         
     return (start_time_sec, duration)
 
+class PlanetLabSliceInfo(GENISliceInfo):
+    """
+    Subclasses L{GENIAggregateSliceInfo} to specify additional attributes
+    for PlanetLab RSpecs
+    """
+    start_time = models.DateTimeField(blank=True, null=True)
+    end_time = models.DateTimeField(blank=True, null=True)
+
 class PlanetLabNetwork(Resource):
     """
     PlanetLab NetSpec.
     """
+    
+    @classmethod
+    def get_from_elem(cls, pl_aggregate, network_elem):
+        """
+        Create or update the NetSpec in the DB from the Element C{network_elem}
+        that is found in an RSpec. If the same name is in another aggregate,
+        a L{NameConflictException} is raised.
+        
+        @param pl_aggregate: The PlanetLabAggregate to which this network
+            belongs.
+        @type pl_aggregate: L{PlanetLabAggregate}
+        @param network_elem: a NetSpec element from an RSpec.
+        @type network_elem: C{xml.etree.ElementTree.Element}
+        """
+        name = network_elem.get("name")
+        
+        if not name:
+            raise RSpecParsingException("Could not find NetSpec name.")
+        
+        try:
+            net = cls.objects.get(name=name)
+        except cls.DoesNotExist:
+            net = cls.objects.create(
+                name=name, aggregate=pl_aggregate)
+            
+        if net.aggregate.pk != pl_aggregate.pk:
+            raise NameConflictException(
+                "Network %s is in a different aggregate." % name)
+        
+        if net.available == False:
+            net.available = True
+            net.update_timestamp()
+        
+        net.save()
+        
+        # Update the nodes in the network
+        current_node_pks = set(
+            net.nodes.filter(available=True).values_list("pk", flat=True))
+        new_node_pks = set()
+        node_elems = network_elem.findall(".//NodeSpec")
+        for node_elem in node_elems:
+            node = PlanetLabNode.get_from_elem(net, node_elem)
+            new_node_pks.add(node.pk)
+        
+        # set available to False for old nodes
+        old_node_pks = current_node_pks - new_node_pks
+        for pk in old_node_pks:
+            node = PlanetLabNode.objects.get(pk=pk)
+            node.available = False
+            node.update_timestamp()
+            node.save()
+            
+        return net
     
     def get_as_elem(self, slice=None):
         """
@@ -152,6 +206,62 @@ class PlanetLabNode(Resource):
     disk_max = models.IntegerField()
     network = models.ForeignKey(PlanetLabNetwork, related_name="nodes")
     
+    @classmethod
+    def get_from_elem(cls, network, node_elem):
+        """
+        Create or update the planetlab node in the DB from the Element
+        C{node_elem} that is found in an RSpec. If the same name is in another
+        aggregate, a L{NameConflictException} is raised.
+        
+        @param network: The PlanetLabNetwork to which this node belongs.
+        @type network: L{PlanetLabNetwork}
+        @param node_elem: a NodeSpec element from an RSpec.
+        @type node_elem: C{xml.etree.ElementTree.Element}
+        """
+        name = node_elem.get("name")
+        
+        if not name:
+            raise RSpecParsingException("Could not find NodeSpec name.")
+        
+        try:
+            node = cls.objects.get(name=name)
+            if node.aggregate.pk != network.aggregate.pk:
+                raise NameConflictException(
+                    "Node %s is in a different aggregate." % name)
+            if not node.available:
+                node.available = True
+                node.update_timestamp()
+        except cls.DoesNotExist:
+            node = cls(name=name, aggregate=network.aggregate, available=True)
+            
+        for attr in ["type", "init_params"]:
+            setattr(node, attr, node_elem.get(attr, ""))
+            
+        for attr in ["cpu_min", "cpu_share", "cpu_pct", "disk_max"]:
+            setattr(node, attr, node_elem.get(attr, 0))
+        
+        node.network = network
+        
+        node.save()
+        
+        # Update the ifaces on the node
+        current_iface_pks = set(
+            node.interfaces.filter(available=True).values_list("pk", flat=True))
+        new_iface_pks = set()
+        iface_elems = node_elem.findall(".//IfaceSpec")
+        for iface_elem in iface_elems:
+            iface = PlanetLabInterface.get_from_elem(node, iface_elem)
+            new_iface_pks.add(iface.pk)
+        
+        # set available to False for old interfaces
+        old_iface_pks = current_iface_pks - new_iface_pks
+        for pk in old_iface_pks:
+            iface = PlanetLabInterface.objects.get(pk=pk)
+            iface.available = False
+            iface.save()
+
+        return node
+    
     def get_as_elem(self):
         """
         Return the node as an ElementTree element.
@@ -188,8 +298,45 @@ class PlanetLabInterface(models.Model):
     max_kbyte = models.IntegerField()
     ip_spoof = models.BooleanField("Spoof IP Address?")
     
+    available = models.BooleanField(default=True)
+    
 #    link = models.ForeignKey(PlanetLabLink, related_name="endpoints")
-    node = models.ForeignKey(PlanetLabNode)
+    node = models.ForeignKey(PlanetLabNode, related_name="interfaces")
+
+    @classmethod
+    def get_from_elem(cls, node, iface_elem):
+        """
+        Create or update the planetlab interface in the DB from the Element
+        C{iface_elem} that is found in an RSpec.
+        
+        @param node: The PlanetLabNode to which this interface belongs.
+        @type network: L{PlanetLabNode}
+        @param iface_elem: a IfaceSpec element from an RSpec.
+        @type iface_elem: C{xml.etree.ElementTree.Element}
+        """
+        name = iface_elem.get("name")
+        
+        if not name:
+            raise RSpecParsingException("Could not find IfaceSpec name.")
+        
+        try:
+            iface = cls.objects.get(name=name, node=node)
+        except cls.DoesNotExist:
+            iface = cls(name=name, node=node)
+            
+        for attr in ["addr", "type", "init_params"]:
+            setattr(iface, attr, iface_elem.get(attr, ""))
+            
+        for attr in ["min_rate", "max_rate", "max_kbyte"]:
+            setattr(iface, attr, iface_elem.get(attr, 0))
+        
+        for attr in ["ip_spoof"]:
+            setattr(iface, attr, iface_elem.get(attr, False))
+        
+        iface.available = True
+        iface.save()
+        
+        return iface
 
     def get_as_elem(self):
         """
@@ -221,13 +368,19 @@ class PlanetLabNetworkSliver(Sliver):
     start_time = models.DateTimeField()
     end_time = models.DateTimeField()
 
-class PlanetLabAggregate(GENIAggergate):
+class PlanetLabAggregate(GENIAggregate):
     information = \
 """
 A PlanetLab Aggregate exposed through the GENI API.
 """
     
+    class Meta:
+        verbose_name = "PlanetLab GENI-API Aggregate"
+    
     def _to_rspec(self, slice):
+        """
+        See L{GENIAggregate._to_rspec}.
+        """
         info = slice.planetlabsliceinfo
         stime, duration = get_start_duration(info.start_time, info.end_time)
         rspec = et.Element("RSpec", dict(
@@ -239,4 +392,39 @@ A PlanetLab Aggregate exposed through the GENI API.
             rspec.append(net.get_as_elem(slice))
         
         return rspec
+    
+    def _list_resources(self):
+        """
+        See L{GENIAggregate._list_resources}.
+        """
+        rspec = self.proxy.ListResources(
+            [self.get_am_cred()], 
+            {"geni_compressed": False, "geni_available": True})
+        
+        root = et.fromstring(rspec)
+        
+        current_net_pks = set(
+            PlanetLabNetwork.objects.filter(
+                available=True, aggregate__pk=self.pk).values_list(
+                    "pk", flat=True))
+        new_net_pks = set()
+        network_elems = root.findall(".//NetSpec")
+        for network_elem in network_elems:
+            network = PlanetLabNetwork.get_from_elem(self, network_elem)
+            new_net_pks.add(network.pk)
+        
+        # set available to False for old networks
+        old_net_pks = current_net_pks - new_net_pks
+        for pk in old_net_pks:
+            net = PlanetLabNetwork.objects.get(pk=pk)
+            net.available = False
+            net.save()
+
+    def add_to_slice(self, slice, next):
+        """
+        Create a PlanetLabSliceInfo instance for this slice if none exists.
+        """
+        info, created = PlanetLabSliceInfo.objects.get_or_create(slice=slice)
+        slice.aggregate.add(self)
+        return next
     
