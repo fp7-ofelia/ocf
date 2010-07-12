da73e7fe7494a0fda279ebd698a352214ad44927
Jad Naous <jnaous@beirut.stanford.edu>
2010-07-12 11:26:24 -0700

    use agg_id instead of obj_id, make default delete smarter

--- src/python/expedient/clearinghouse/aggregate/views.py
+++ src/python/expedient/clearinghouse/aggregate/views.py
@@ -7,12 +7,14 @@ from expedient.clearinghouse.aggregate.models import Aggregate
 from expedient.clearinghouse.aggregate.forms import AggregateTypeForm
 from django.core.urlresolvers import reverse
 from django.contrib.contenttypes.models import ContentType
+from django.shortcuts import get_object_or_404
+from expedient.common.messaging.models import DatedMessage
 
 TEMPLATE_PATH = "expedient/clearinghouse/aggregate"
 
-def list(request, obj_id=None):
+def list(request, agg_id=None):
     '''
-    Get a list of aggregates. obj_id specifies id to highlight. On POST,
+    Get a list of aggregates. agg_id specifies id to highlight. On POST,
     get the type of aggregate to be created and redirect to that model's
     create url.
     '''
@@ -36,18 +38,27 @@ def list(request, obj_id=None):
         template_object_name="aggregate",
         extra_context={
             'form': form,
-            'highlight_id': obj_id,
+            'highlight_id': agg_id,
         },
     )
 
-def delete(request, obj_id):
-    return create_update.delete_object(
+def delete(request, agg_id):
+    next = request.GET.get("next", None)
+    aggregate = get_object_or_404(Aggregate, id=agg_id)
+    req = create_update.delete_object(
         request,
         model=Aggregate,
-        post_delete_redirect=reverse("aggregate_all"),
-        object_id=obj_id,
+        post_delete_redirect=next,
+        object_id=agg_id,
+        extra_context={"next": next},
         template_name=TEMPLATE_PATH+"/delete.html",
     )
+    if req.status_code == HttpResponseRedirect.status_code:
+        DatedMessage.objects.post_message_to_user(
+            "Successfully deleted aggregate %s" % aggregate.name,
+            request.user, msg_type=DatedMessage.TYPE_ERROR,
+        )
+    return req
 
 def info(request, ct_id):
     try:
