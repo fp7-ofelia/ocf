1c6b06f240d4388b3e140e3d9ab610711616f539
Jad Naous <jnaous@beirut.stanford.edu>
2010-07-12 11:27:41 -0700

    Add functions to manage status change timestamp better

--- src/python/expedient/clearinghouse/resources/models.py
+++ src/python/expedient/clearinghouse/resources/models.py
@@ -5,6 +5,7 @@ from django.db import models
 from expedient.clearinghouse.aggregate.models import Aggregate
 from expedient.common.extendable.models import Extendable
 from expedient.clearinghouse.slice.models import Slice
+from datetime import datetime
 
 class Resource(Extendable):
     '''
@@ -18,16 +19,20 @@ class Resource(Extendable):
     
     name = models.CharField(max_length=200)
     available = models.BooleanField("Available", default=True, editable=False)
-    status_change_timestamp = models.DateTimeField(editable=False)
+    status_change_timestamp = models.DateTimeField(
+        editable=False, auto_now_add=True)
     aggregate = models.ForeignKey(
         Aggregate, verbose_name="Aggregate the resource belongs to")
     slice_set = models.ManyToManyField(
         Slice, through="Sliver", verbose_name="Slices this resource is used in")
     
+    def update_timestamp(self):
+        self.status_change_timestamp = datetime.now()
+    
     def __unicode__(self):
         return u"Resource: %s belonging to aggregate %s." % (
             self.name, self.aggregate)
-
+        
 class Sliver(Extendable):
     '''
     Information on the reservation of a particular resource for a slice.
