7e6dcbf6eb121dd104ea8a86fedd73726414c0ce
Jad Naous <jnaous@beirut.stanford.edu>
2010-07-13 16:41:01 -0700

    fix messages

--- src/python/gcf/geni/ch.py
+++ src/python/gcf/geni/ch.py
@@ -257,8 +257,8 @@ class Clearinghouse(object):
 #                pass
             
         if serverstr == "":
-            self.logger.error('Failed to generate complete user cert using trusted CAs. For user %s didnt find issuer %s cert', user_cert.get_subject(), user_cert.get_issuer())            
-            raise Exception('Failed to generate complete user cert using trusted CAs. For user %s didnt find issuer %s cert', user_cert.get_subject(), user_cert.get_issuer())
+            self.logger.error('Failed to generate complete user cert using trusted CAs. For user %s didnt find issuer %s cert' % (user_cert.get_subject(), user_cert.get_issuer())            
+            raise Exception('Failed to generate complete user cert using trusted CAs. For user %s didnt find issuer %s cert' % (user_cert.get_subject(), user_cert.get_issuer()))
             # use the commandline user cert for test purposes? Raise an exception?
             # HACK!
             user_gid = gid.GID(filename=self.usercert)
