f3ca58f9af390958e05fdee94a40dca88b0988aa
Jad Naous <jnaous@beirut.stanford.edu>
2010-07-13 21:21:38 -0700

    updated scripts to run gam and gch

--- bin/run_gam.sh
+++ bin/run_gam.sh
@@ -4,6 +4,6 @@ source expedient-settings
 
 echo Starting the GAM in a detached screen named gam...
 # Run the GAM
-screen -S "gam" -dm /usr/bin/python /home/expedient/expedient/src/python/gcf/gam.py -u https://localhost:443/openflow/gapi/ -r $GAPI_SSL_DIR/ca.crt -c $GAPI_SSL_DIR/server.crt -k $GAPI_SSL_DIR/server.key -p $GAM_PORT --debug -H 0.0.0.0 2>&1 | tee ~/gam.log
+screen -S "gam" -dm /usr/bin/python /home/expedient/expedient/src/python/gcf/gam.py -u https://localhost:443/openflow/gapi/ -u $GAPI_SSL_DIR/experimenter.crt -r $GAPI_SSL_DIR -c $GAPI_SSL_DIR/server.crt -k $GAPI_SSL_DIR/server.key -p $GAM_PORT --debug -H 0.0.0.0 2>&1 | tee ~/gam.log
 echo To access the screen, use the command: screen -rd gam
 echo Output is being logged to ~/gam.log
--- bin/run_gch.sh
+++ bin/run_gch.sh
@@ -4,7 +4,7 @@ source expedient-settings
 
 echo Starting the GCH in a detached screen named 'gch'...
 # Run the GCH
-screen -S "gch" -dm /usr/bin/python /home/expedient/expedient/src/python/gcf/gch.py -r $GAPI_SSL_DIR/ca.crt -c $GAPI_SSL_DIR/ch.crt -k $GAPI_SSL_DIR/ch.key -p $GCH_PORT --debug -H 0.0.0.0 2>&1 | tee ~/gch.log
+screen -S "gch" -dm /usr/bin/python /home/expedient/expedient/src/python/gcf/gch.py -r $GAPI_SSL_DIR -c $GAPI_SSL_DIR/ch.crt -k $GAPI_SSL_DIR/ch.key -p $GCH_PORT --debug -H 0.0.0.0 2>&1 | tee ~/gch.log
 echo To access the screen, use the command: screen -rd gch
 echo Output is being logged to ~/gch.log
 
