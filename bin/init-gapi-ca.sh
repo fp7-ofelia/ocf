41b6272cb3b84224ba64609d8b3c5b8cb3409d5f
Jad Naous <jnaous@beirut.stanford.edu>
2010-07-14 04:43:59 -0700

    put certs in a separate dir

--- bin/init-gapi-ca.sh
+++ bin/init-gapi-ca.sh
@@ -3,5 +3,7 @@
 source expedient-settings
 
 # initialize the CA used for the GENI API
-mkdir -p $GAPI_SSL_DIR
+mkdir -p $GAPI_SSL_DIR/certs
 python /home/expedient/expedient/src/python/gcf/init-ca.py -d $GAPI_SSL_DIR --am --ch --ca --exp
+ln -s $GAPI_SSL_DIR/*.crt /$GAPI_SSL_DIR/certs/
+
