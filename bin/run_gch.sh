15d935224a4fd7840e8e28c34ab653a9eed60d0f
Jad Naous <jnaous@beirut.stanford.edu>
2010-07-14 05:46:37 -0700

    fix locations

--- bin/run_gch.sh
+++ bin/run_gch.sh
@@ -4,7 +4,6 @@ source expedient-settings
 
 echo Starting the GCH in a detached screen named 'gch'...
 # Run the GCH
-screen -S "gch" -dm /usr/bin/python /home/expedient/expedient/src/python/gcf/gch.py -u $GAPI_SSL_DIR/experimenter.crt -r $GAPI_SSL_DIR -c $GAPI_SSL_DIR/ch.crt -k $GAPI_SSL_DIR/ch.key -p $GCH_PORT --debug -H 0.0.0.0 2>&1 | tee ~/gch.log
+screen -S "gch" -dm /usr/bin/python /home/expedient/expedient/src/python/gcf/gch.py -u $GAPI_SSL_DIR/experimenter.crt -r $GAPI_SSL_DIR/certs -c $GAPI_SSL_DIR/ch.crt -k $GAPI_SSL_DIR/ch.key -p $GCH_PORT --debug -H 0.0.0.0 2>&1 | tee ~/gch.log
 echo To access the screen, use the command: screen -rd gch
 echo Output is being logged to ~/gch.log
-
