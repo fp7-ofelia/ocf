1dfa0f9bfa6c3c6d199739b8df489f9ad38eb10a
Jad Naous <jnaous@beirut.stanford.edu>
2010-07-15 05:23:33 -0700

    fix url param

--- src/python/expedient/clearinghouse/aggregate/urls.py
+++ src/python/expedient/clearinghouse/aggregate/urls.py
@@ -7,6 +7,6 @@ urlpatterns = patterns('expedient.clearinghouse.aggregate.views',
     url(r'^list/$', 'list', name='aggregate_all'),
     url(r'^list/(?P<agg_id>\d+)/$', 'list', name='aggregate_list_highlight'),
     url(r'^delete/(?P<agg_id>\d+)/$', 'delete', name='aggregate_delete'),
-    url(r'^info/(?P<agg_id>\d+)/$', 'info', name='aggregate_info'),
+    url(r'^info/(?P<ct_id>\d+)/$', 'info', name='aggregate_info'),
 )
 
