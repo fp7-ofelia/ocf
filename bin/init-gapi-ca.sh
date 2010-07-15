310674c4db182b44a69c9f749dadf03a3036b686
Jad Naous <jnaous@beirut.stanford.edu>
2010-07-15 05:22:21 -0700

    fix rm options

--- bin/init-gapi-ca.sh
+++ bin/init-gapi-ca.sh
@@ -5,7 +5,8 @@ source expedient-settings
 # initialize the CA used for the GENI API
 mkdir -p $GAPI_SSL_DIR/certs
 python /home/expedient/expedient/src/python/gcf/init-ca.py -d $GAPI_SSL_DIR --am --ch --ca --exp
-rm -s $GAPI_SSL_DIR/ca.crt
+rm -f $GAPI_SSL_DIR/ca.crt
 ln -s $GAPI_SSL_DIR/ca.crt /$GAPI_SSL_DIR/certs/
-rm -s $GAPI_SSL_DIR/ch.crt
+rm -f $GAPI_SSL_DIR/ch.crt
 ln -s $GAPI_SSL_DIR/ch.crt /$GAPI_SSL_DIR/certs/
+
