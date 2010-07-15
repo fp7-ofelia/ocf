885b8b6e855ceb028b132f5495590a6a7c27b0d6
Jad Naous <jnaous@beirut.stanford.edu>
2010-07-15 05:24:52 -0700

    Add planetlab to HTML UI

--- src/python/expedient/ui/html/urls.py
+++ src/python/expedient/ui/html/urls.py
@@ -8,4 +8,7 @@ from django.conf.urls.defaults import patterns, url
 urlpatterns = patterns('expedient.ui.html.views',
     url(r'^(?P<slice_id>\d+)/$', 'home', name='html_plugin_home'),
     url(r'^flowspace/(?P<slice_id>\d+)/$', 'flowspace', name='html_plugin_flowspace'),
+    url(r'^ssh_keys/(?P<slice_id>\d+)/$', 'sshkeys', name='html_plugin_sshkeys'),
+    url(r'^ssh_public_key/(?P<slice_id>\d+)/$', 'sshkey_file', kwargs={"type": "ssh_public_key"}, name='html_plugin_sshkeys_public'),
+    url(r'^ssh_private_key/(?P<slice_id>\d+)/$', 'sshkey_file', kwargs={"type": "ssh_private_key"}, name='html_plugin_sshkeys_private'),
 )
--- src/python/expedient/ui/html/views.py
+++ src/python/expedient/ui/html/views.py
@@ -8,36 +8,73 @@ from django.shortcuts import get_object_or_404
 from expedient.clearinghouse.slice.models import Slice
 from openflow.plugin.models import OpenFlowAggregate, OpenFlowSwitch,\
     OpenFlowInterface, OpenFlowInterfaceSliver, FlowSpaceRule
-from django.http import HttpResponseRedirect, HttpResponseNotAllowed
+from django.http import HttpResponseRedirect, HttpResponseNotAllowed,\
+    HttpResponse
 from expedient.common.messaging.models import DatedMessage
 from django.forms.models import inlineformset_factory
 from django.core.urlresolvers import reverse
 from models import SliceFlowSpace
+from geni.planetlab.models import PlanetLabNode, PlanetLabSliver,\
+    PlanetLabAggregate
 
 import logging
 logger = logging.getLogger("html_ui_views")
 
+def _update_openflow_resources(request, slice):
+    """
+    Process the request to add/remove openflow resources from slice.
+    """
+    iface_ids = map(int, request.POST.getlist("selected_ifaces"))
+    ifaces = OpenFlowInterface.objects.filter(id__in=iface_ids)
+    # TODO: Send message if id not found.
+    # get or create slivers for the ifaces in the slice.
+    for iface in ifaces:
+        # make sure all the selected interfaces are added
+        sliver, created = OpenFlowInterfaceSliver.objects.get_or_create(
+            slice=slice, resource=iface)
+        if created:
+            DatedMessage.objects.post_message_to_user(
+                "Successfully added interface %s to slice %s" % (
+                    iface, slice.name),
+                request.user, msg_type=DatedMessage.TYPE_SUCCESS)
+    # Delete all unselected interfaces
+    to_del = OpenFlowInterfaceSliver.objects.exclude(
+        resource__id__in=iface_ids).filter(slice=slice)
+    to_del.delete()
+    
+def _update_planetlab_resources(request, slice):
+    """
+    Process the request to add/remove planetlab resources from slice.
+    """
+    node_ids = map(int, request.POST.getlist("selected_nodes"))
+    nodes = PlanetLabNode.objects.filter(id__in=node_ids)
+    # TODO: Send message if id not found.
+    # get or create slivers for the nodes in the slice.
+    for node in nodes:
+        # make sure all the selected interfaces are added
+        sliver, created = PlanetLabSliver.objects.get_or_create(
+            slice=slice, resource=node)
+        if created:
+            DatedMessage.objects.post_message_to_user(
+                "Successfully added %s to slice %s" % (
+                    node, slice.name),
+                request.user, msg_type=DatedMessage.TYPE_SUCCESS)
+    # Delete all unselected interfaces
+    to_del = PlanetLabSliver.objects.exclude(
+        resource__id__in=node_ids).filter(slice=slice)
+    to_del.delete()
+
 def home(request, slice_id):
+    """
+    Display the list of planetlab and openflow aggregates and their resources.
+    On submit, create slivers and make reservation.
+    """
+    
     slice = get_object_or_404(Slice, id=slice_id)
     if request.method == "POST":
-        iface_ids = map(int, request.POST.getlist("selected_ifaces"))
-        ifaces = OpenFlowInterface.objects.filter(id__in=iface_ids)
-        # TODO: Send message if id not found.
-        # get or create slivers for the ifaces in the slice.
-        for iface in ifaces:
-            # make sure all the selected interfaces are added
-            sliver, created = OpenFlowInterfaceSliver.objects.get_or_create(
-                slice=slice, resource=iface)
-            if created:
-                DatedMessage.objects.post_message_to_user(
-                    "Successfully added interface %s to slice %s" % (
-                        iface, slice.name),
-                    request.user, msg_type=DatedMessage.TYPE_SUCCESS)
-        # Delete all unselected interfaces
-        to_del = OpenFlowInterfaceSliver.objects.exclude(
-            resource__id__in=iface_ids).filter(slice=slice)
-        for s in to_del:
-            s.delete()
+        
+        _update_openflow_resources(request, slice)
+        _update_planetlab_resources(request, slice)
         
         slice.modified = True
         slice.save()
@@ -46,19 +83,22 @@ def home(request, slice_id):
         return HttpResponseRedirect(reverse("html_plugin_flowspace",
                                             args=[slice_id]))
     else:
-        checked_ids = slice.resource_set.filter_for_objects(
-            OpenFlowInterface, slice_set=slice).values_list("id", flat=True)
+        checked_ids = list(slice.resource_set.filter_for_objects(
+            OpenFlowInterface, slice_set=slice).values_list("id", flat=True))
+        checked_ids.extend(slice.resource_set.filter_for_objects(
+            PlanetLabNode, slice_set=slice).values_list("id", flat=True))
         logger.debug("Interfaces in slice: %s" % checked_ids)
         return simple.direct_to_template(
             request,
             template="html/select_resources.html",
             extra_context={
-                "openflow_aggs": OpenFlowAggregate.objects.filter(
-                    slice=slice,
-                ),
+                "openflow_aggs": OpenFlowAggregate.objects.filter(slice=slice),
+                "planetlab_aggs": PlanetLabAggregate.objects.filter(
+                    slice=slice),
                 "slice": slice,
                 "checked_ids": checked_ids,
                 "ofswitch_class": OpenFlowSwitch,
+                "planetlab_node_class": PlanetLabNode,
                 "breadcrumbs": (
                     ("Home", reverse("home")),
                     ("Project %s" % slice.project.name, reverse("project_detail", args=[slice.project.id])),
@@ -107,7 +147,7 @@ def flowspace(request, slice_id):
             )
             
             return HttpResponseRedirect(
-                reverse("slice_detail", args=[slice_id]))
+                reverse("html_plugin_sshkeys", args=[slice_id]))
         else:
             logger.debug("Flowspace invalid: %s" % formset)
     elif request.method == "GET":
@@ -130,3 +170,46 @@ def flowspace(request, slice_id):
             ),
         },
     )
+
+def sshkeys(request, slice_id):
+    """
+    Show links to download ssh keys. Regenerate keys on submit.
+    """
+    slice = get_object_or_404(Slice, id=slice_id)
+    
+    if request.method == "POST":
+        slice.geni_slice_info.generate_ssh_keys()
+        return HttpResponseRedirect(request.path)
+    
+    return simple.direct_to_template(
+        request,
+        template="html/sshkeys.html",
+        extra_context={
+            "slice":slice,
+            "breadcrumbs": (
+                ("Home", reverse("home")),
+                ("Project %s" % slice.project.name, reverse("project_detail", args=[slice.project.id])),
+                ("Slice %s" % slice.name, reverse("slice_detail", args=[slice_id])),
+                ("HTML UI - Choose Resources", reverse("html_plugin_home", args=[slice_id])),
+                ("HTML UI - Choose Flowspace", reverse("html_plugin_flowspace", args=[slice_id])),
+                ("HTML UI - Download SSH Keys", reverse("html_plugin_sshkeys", args=[slice_id])),
+            ),
+        }
+    )
+
+def sshkey_file(request, slice_id, type):
+    """
+    Send a file.
+    """
+    slice = get_object_or_404(Slice, id=slice_id)
+    if type != "ssh_public_key" and type != "ssh_private_key":
+        raise Exception("Unknown request for file %s" % type)
+    
+    data = getattr(slice.geni_slice_info, type)
+    
+    filename = "id_rsa"
+    if type == "ssh_public_key": filename = filename + ".pub"
+    
+    response = HttpResponse(data, mimetype="application/x-download")
+    response["Content-Disposition"] = 'attachment;filename=%s' % filename
+    return response
