5bf17703e1397b0ffd0ef2b09c438720bf5dcd2b
Jad Naous <jnaous@beirut.stanford.edu>
2010-07-15 05:28:06 -0700

    fix a lot of things...

--- src/python/geni/models.py
+++ src/python/geni/models.py
@@ -12,6 +12,8 @@ import xmlrpclib
 from expedient.clearinghouse.slice.models import Slice
 from geni import management
 
+SSH_KEY_SIZE = 2048
+
 class GENISliceInfo(models.Model):
     """
     Holds information about the slice that's relevant for the GENI API.
@@ -21,17 +23,66 @@ class GENISliceInfo(models.Model):
     @ivar slice_urn: the slice's unique resource name. This will be
         automatically created in the __init__ function if not given.
     @type slice_urn: a string.
+    @ivar ssh_public_key: Public ssh key in base64. Can be None.
+    @type ssh_public_key: string or None.
+    @ivar ssh_private_key: Private ssh key in base64, possibly encrypted.
+        See L{generate_ssh_keys}. Can be None.
+    @type ssh_private_key: string or None.
+    @ivar slice_cred: Credentials to use for the slice
+    @type slice_cred: string
     """
     slice = models.OneToOneField(
         Slice, related_name="geni_slice_info")
     slice_urn = models.CharField(max_length=1024)
+    ssh_public_key = models.TextField(
+        "Slice's SSH public key", editable=False, blank=True, null=True)
+    ssh_private_key = models.TextField(
+        "Slice's SSH private key", editable=False, blank=True, null=True)
+    slice_cred = models.TextField(
+        "Slice credentials", editable=False, blank=True, null=True)
     
     def __init__(self, *args, **kwargs):
         kwargs.setdefault("slice_urn", management.create_slice_urn())
         super(GENISliceInfo, self).__init__(*args, **kwargs)
         
+    def generate_ssh_keys(self, password=None):
+        """
+        Set the C{ssh_public_key} and C{ssh_private_key} attributes to be
+        new keys. Note that the keys are stored in base64.
+        
+        @parameter password: password to use to encrypt the private key
+        @type password: string
+        """
+        from paramiko import RSAKey
+        from StringIO import StringIO
+        
+        key = RSAKey.generate(SSH_KEY_SIZE)
+        
+        output = StringIO()
+        key.write_private_key(output, password=password)
+        self.ssh_private_key = output.getvalue()
+        output.close()
+        
+        self.ssh_public_key = \
+            "ssh-rsa %s auto-generated Expedient key" % (key. get_base64())
+        
+        
+    def generate_slice_cred(self):
+        """
+        Create credentials to use for the slice.
+        """
+        from gcf.sfa.trust import gid
+        slice_gid, keys = management.create_slice_gid(self.slice_urn)
+        user_gid = gid.GID(filename=settings.GCF_X509_CH_CERT)
+        cred = management.create_slice_credential(user_gid, slice_gid)
+        self.slice_cred = cred.save_to_string()
+    
 class GENIAggregate(Aggregate):
-    url = models.URLField(max_length=200)
+    """
+    Implements a generic GCF-based aggregate manager to simplify adding
+    GCF-based plugins.
+    """
+    url = models.CharField(max_length=200)
     
     class URLNotDefined(Exception): pass
     
@@ -50,6 +101,9 @@ class GENIAggregate(Aggregate):
                 certfile=settings.GCF_X509_CH_CERT)
     
             self.proxy = xmlrpclib.ServerProxy(u, transport=transp)
+            return self.proxy
+        else:
+            raise AttributeError, name
     
     @classmethod
     def get_am_cred(cls):
@@ -58,25 +112,10 @@ class GENIAggregate(Aggregate):
         
         @return: GENI credential string.
         """
-        if hasattr(self, "am_cred"):
-            return  cls.am_cred
-        
         f = open(settings.GCF_NULL_SLICE_CRED)
-        cls.am_cred = f.read()
+        am_cred = f.read()
         f.close()
-        return cls.am_cred
-    
-    def _to_rspec(self, slice):
-        """
-        Change this slice into an rspec for this aggregates
-        """
-        raise NotImplementedError()
-    
-    def _list_resources(self):
-        """
-        Parse the rspec and update resources in the database.
-        """
-        raise NotImplementedError()
+        return am_cred
     
     def _create_sliver(self, slice):
         """
@@ -84,12 +123,12 @@ class GENIAggregate(Aggregate):
         Creates a sliver on the aggregate from this slice.
         """
         
-        rspec = self.as_leaf_class().to_rspec(slice)
+        rspec = self.as_leaf_class()._to_rspec(slice)
         info = slice.geni_slice_info
         
         try:
             reserved = self.proxy.CreateSliver(
-                info.slice_urn, [self.get_am_cred()], rspec)
+                info.slice_urn, [info.slice_cred], rspec, [{"key": [info.ssh_public_key]}])
         except Exception as e:
             import traceback
             traceback.print_exc()
@@ -102,13 +141,10 @@ class GENIAggregate(Aggregate):
         Corresponds to the DeleteSliver call of the GENI aggregate API.
         Stop and delete slice at the aggregate.
         """
-        
-        rspec = self.as_leaf_class().to_rspec(slice)
         info = slice.geni_slice_info
-        
         try:
             ret = self.proxy.DeleteSliver(
-                info.slice_urn, [self.get_am_cred()], rspec)
+                info.slice_urn, [info.slice_cred])
         except Exception as e:
             import traceback
             traceback.print_exc()
@@ -124,8 +160,10 @@ class GENIAggregate(Aggregate):
         # TODO: fill up
         raise NotImplementedError()
         
-    ####################################################################
-    # Overrides from expedient.clearinghouse.aggregate.models.Aggregate
+    #####################################################################
+    # Overrides from expedient.clearinghouse.aggregate.models.Aggregate #
+    #####################################################################
+
     def check_status(self):
         try:
             ver = self.proxy.GetVersion()
@@ -135,16 +173,41 @@ class GENIAggregate(Aggregate):
             return False
         return self.available
     
-    def add_to_slice(self, slice, next):
-        """
-        Create a GENISliceInfo instance for this slice if none exists.
-        """
-        info, created = GENISliceInfo.objects.get_or_create(slice=slice)
-        slice.aggregate.add(self)
-        return next
-    
     def start_slice(self, slice):
-        return self.create_sliver(slice)
+        return self._create_sliver(slice)
         
     def stop_slice(self, slice):
-        return self.delete_sliver(slice)
+        return self._delete_sliver(slice)
+    
+    def add_to_slice(self, slice, next):
+        """
+        Make sure that there's only one GENI slice info.
+        """
+        info, created = GENISliceInfo.objects.get_or_create(
+            slice=slice,
+        )
+        if created:
+            info.generate_ssh_keys()
+            info.generate_slice_cred()
+            info.save()
+            
+        slice.aggregates.add(self)
+            
+        return next
+
+    #####################################################################
+    # Subclasses must override the below functions                      #
+    #####################################################################
+    
+    def _to_rspec(self, slice):
+        """
+        Change this slice into an rspec for this aggregates
+        """
+        raise NotImplementedError()
+    
+    def update_resources(self):
+        """
+        Parse the rspec and update resources in the database.
+        """
+        raise NotImplementedError()
+    
