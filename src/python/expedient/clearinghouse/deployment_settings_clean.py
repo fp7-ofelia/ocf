b90dd3ccf3870c0440847653c6c65cc7d709882b
Jad Naous <jnaous@beirut.stanford.edu>
2010-07-16 12:44:58 -0700

    remove deployment files and update clean settings files

--- src/python/expedient/clearinghouse/deployment_settings_clean.py
+++ src/python/expedient/clearinghouse/deployment_settings_clean.py
@@ -25,15 +25,20 @@ DEFAULT_FROM_EMAIL = 'no-reply@geni.org'
 EMAIL_SUBJECT_PREFIX = '[GENI-Clearinghouse] '
 
 # XML-RPC settings
-MY_CA = join('/etc/apache2/ssl.crt/ca.crt')
+MY_CA = join('/etc/apache2/ssl.crt', 'ca.crt')
 
 # default site
 SITE_NAME = "Expedient Clearinghouse"
 SITE_DOMAIN = "clearinghouse.geni.org" # example
 
 # Openflow GAPI settings
+# Below is the URN prefix used to identify this site. You will need to
+# change this to your deployment.
 OPENFLOW_GAPI_RSC_URN_PREFIX = "urn:publicid:IDN+openflow:stanford"
-OPENFLOW_GAPI_AM_URN = "urn:publicid:IDN+openflow:stanford+authority+am"
+# This is the URN of the aggregate manager. Default usually sufficient.
+OPENFLOW_GAPI_AM_URN = OPENFLOW_GAPI_RSC_URN_PREFIX+"+authority+am"
+# A list of the Names of OpenFlow aggregates that you do not want to
+# show for the ListResources call.
 OPENFLOW_GAPI_FILTERED_AGGS = []
 
 # The domain name in URNs for Expedient acting as a Clearinghouse.
