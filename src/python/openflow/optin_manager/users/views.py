c71e720ac5370bde5c9c3ea071c653e4885db171
Peyman Kazemian <kazemian@stanford.edu>
2010-07-19 02:04:22 -0700

    changed views

--- src/python/openflow/optin_manager/users/views.py
+++ src/python/openflow/optin_manager/users/views.py
@@ -4,7 +4,7 @@ from django.contrib.auth.decorators import login_required
 from django.http import *
 from django.http import HttpResponse
 from django.views.generic import simple
-from openflow.optin_manager.opts.models import AdminFlowSpace, UserFlowSpace
+from openflow.optin_manager.opts.models import AdminFlowSpace, UserFlowSpace, UserOpts
 from openflow.optin_manager.admin_manager.models import *
 from openflow.optin_manager.users.models import Priority
 
@@ -28,23 +28,38 @@ def dashboard(request):
         if len(list)==0:
             AdminFlowSpace.objects.create(user=request.user)
     
-    profile = UserProfile.get_or_create_profile(request.user)
-    username = profile.user.username
+    profile = UserProfile.get_or_create_profile(request.user)    
     
-    next_steps = []
-#    if (not profile.is_net_admin):
-#        ufs = UserFlowSpace.objects.filter(user=request.user)
-#        if (len(ufs) == 0):
-#            rufs = RequestedUserFlowSpace.objects.filter(user=request.user)
-#        else:
-#            next_steps.append("")
+    if (not profile.is_net_admin):
+        next_steps = 0
+        opts = []
+        ufs = UserFlowSpace.objects.filter(user=request.user)
+        if (len(ufs) == 0):
+            rufs = RequestedUserFlowSpace.objects.filter(user=request.user)
+            if (len(rufs) == 0):
+                next_steps = 0
+            else:
+                next_steps = 1
+        else:
+            opts = UserOpts.objects.filter(user=request.user).order_by('-priority')
+            if (len(opts) == 0):
+                next_steps = 2
+            else:
+                next_steps = 3
     
-    return simple.direct_to_template(request, 
-                            template = 'openflow/optin_manager/dashboard.html',
+        return simple.direct_to_template(request, 
+                            template = 'openflow/optin_manager/dashboard_user.html',
                             extra_context = {
                                     'user':request.user,
-                                    'profile': profile, 
-                                    'username': username,
                                     'next_steps':next_steps,
+                                    'opts': opts,
+                            },
+                        )
+    else: #Admin
+        
+        return simple.direct_to_template(request, 
+                            template = 'openflow/optin_manager/dashboard_admin.html',
+                            extra_context = {
+                                    'user':request.user,
                             },
                         )
