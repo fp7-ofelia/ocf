df783c30d700f04e498fb34a88ccc3c144fb3c17
Jad Naous <jnaous@beirut.stanford.edu>
2010-07-27 19:21:56 -0700

    interim

---
+++ src/python/expedient/clearinghouse/permissionmgmt/models.py
@@ -0,0 +1,5 @@
+'''
+Created on Jul 27, 2010
+
+@author: jnaous
+'''
---
+++ src/python/expedient/clearinghouse/permissionmgmt/views.py
@@ -0,0 +1,13 @@
+'''
+Created on Jul 27, 2010
+
+@author: jnaous
+'''
+
+def permissions_dashboard(request):
+    """
+    Display a list of current requests and buttons to approve/deny. Also
+    display table of user's permissions.
+    """
+    
+    
