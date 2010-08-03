08622088789896fa056e2643f4aa05d0aed3d2f2
Jad Naous <jnaous@stanford.edu>
2010-08-03 00:25:06 -0700

    refactor to split up contenttype queryset from actual models

--- src/python/expedient/clearinghouse/aggregate/utils.py
+++ src/python/expedient/clearinghouse/aggregate/utils.py
@@ -6,14 +6,25 @@ Created on Aug 2, 2010
 from django.conf import settings
 from django.contrib.contenttypes.models import ContentType
 
-def get_aggregate_types():
+def get_aggregate_classes():
     """
-    Return the ContentType queryset of installed plugin aggregate models.
+    Get the list of modules that are the aggregate classes
+    of aggregate plugins.
     """
     agg_plugin_names = getattr(settings, "AGGREGATE_PLUGINS", [])
     l = []
     for n in agg_plugin_names:
         mod, _, name = n.rpartition(".")
         mod = __import__(mod, fromlist=[name])
-        l.append(ContentType.objects.get_for_model(getattr(mod, name)).id)
+        print mod
+        print name
+        l.append(getattr(mod, name))
+    return l
+
+def get_aggregate_types():
+    """
+    Return the ContentType queryset of installed plugin aggregate models.
+    """
+    l = get_aggregate_classes()
+    l = [ContentType.objects.get_for_model(m).id for m in l]
     return ContentType.objects.filter(id__in=l)
