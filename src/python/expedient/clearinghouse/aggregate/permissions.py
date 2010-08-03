784aa4bd9b68ff71434c7636c2c4363813727e68
Jad Naous <jnaous@stanford.edu>
2010-08-03 11:44:01 -0700

    create the can_use_aggregate permission

--- src/python/expedient/clearinghouse/aggregate/permissions.py
+++ src/python/expedient/clearinghouse/aggregate/permissions.py
@@ -6,6 +6,7 @@ Created on Jun 2, 2010
 from expedient.common.permissions.shortcuts import create_permission
 from expedient.clearinghouse.permissionmgmt.utils import \
     request_permission_wrapper
+from expedient.clearinghouse.aggregate.views import get_can_use_permission
 
 create_permission(
     "can_add_aggregate",
@@ -18,4 +19,12 @@ create_permission(
         "Owners of this permission can edit or delete "
         "the related aggregates in Expedient.",
     view=request_permission_wrapper,
+)
+create_permission(
+    "can_use_aggregate",
+    description=\
+        "Projects, slices, and users that are owners of this permission "
+        "can use the aggregate by starting or stopping slices on it, and "
+        "by calling other methods.",
+    view=get_can_use_permission,
 )
