6f62086986388475dfcc85742266bb72615126c7
Jad Naous <jnaous@beirut.stanford.edu>
2010-08-03 19:10:44 -0700

    Started modifying the tests

--- src/python/expedient/clearinghouse/project/tests.py
+++ src/python/expedient/clearinghouse/project/tests.py
@@ -4,25 +4,46 @@ Created on Jun 11, 2010
 @author: jnaous
 '''
 
+import logging
 from expedient.common.tests.manager import SettingsTestCase
 from django.core.urlresolvers import reverse
 from django.contrib.auth.models import User
 from models import Project
 from pyquery import PyQuery as pq
+from expedient.common.middleware import threadlocals
+from expedient.clearinghouse.aggregate.models import Aggregate
 
-import logging
 logger = logging.getLogger("ProjectTests")
 
-class Tests(SettingsTestCase):
+class TestModels(SettingsTestCase):
     def setUp(self):
-        # Add the user
-        logger.debug("Creating users.")
-        for i in xrange(2):
-            u = User(username="user%s" % i)
-            u.set_password("password")
-            u.save()
+        """Create users and aggregates"""
+        self.su = User.objects.create_superuser(
+            "superuser", "su@su.com", "password")
+        self.u1 = User.objects.create_user(
+            "user1", "u@u.com", "password")
+        self.u2 = User.objects.create_user(
+            "user2", "u@u.com", "password")
         
-        logger.debug("Setup done.")
+        self.client.login(username="superuser", password="password")
+        threadlocals.push_frame(user=self.su)
+        
+        self.agg1 = Aggregate.objects.create(
+            name="Agg1",
+        )
+        self.agg2 = Aggregate.objects.create(
+            name="Agg2",
+        )
+        
+        self.client.logout()
+        threadlocals.pop_frame()
+        
+    def test_allowed_create(self):
+        """Tests that we can create a project"""
+
+        self.client.login(username="superuser", password="password")
+        threadlocals.push_frame(user=self.su)
+        
         
     def test_list(self):
         """
@@ -32,8 +53,7 @@ class Tests(SettingsTestCase):
         members = User.objects.all()[0:2]
         for p in xrange(3):
             l.append(Project.objects.create(
-                name="project"+p, description="description"+p,
-                members=members, owner=members[0]))
+                name="project%s" % p, description="description%s" % p))
         
         # check that the members see 3 projects    
         for i, m in enumerate(members):
