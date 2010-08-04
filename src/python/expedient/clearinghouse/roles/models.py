7e717a9f630070e3d11791884ea02ae55b6c84da
Jad Naous <jnaous@beirut.stanford.edu>
2010-08-03 17:57:20 -0700

    Added roles

---
+++ src/python/expedient/clearinghouse/roles/models.py
@@ -0,0 +1,112 @@
+'''
+Created on Aug 3, 2010
+
+@author: jnaous
+'''
+from django.db import models
+from expedient.clearinghouse.project.models import Project
+from expedient.common.permissions.models import ObjectPermission, Permittee,\
+    PermissionOwnership
+from expedient.common.permissions.shortcuts import delete_permission
+
+class ProjectRole(models.Model):
+    """Groups object permissions together for easier fine-grained management.
+    This role is local to a project.
+    
+    @ivar name: The name of the role. Doesn't needs to be unique within a
+        project.
+    @type name: str, max length=100
+    @ivar project: The project for this role.
+    @type project: L{Project}
+    @ivar obj_permissions: object permissions that this roles groups.
+    @type obj_permissions: C{ManyToManyField} to L{ObjectPermission}.
+    @ivar permittees: Set of permittees that have this role.
+    @type permittees: C{ManyToManyField} to L{Permittee}.
+    """
+    
+    name = models.CharField(max_length=100)
+    project = models.ForeignKey(Project)
+    obj_permissions = models.ManyToManyField(ObjectPermission)
+    permittees = models.ManyToManyField(Permittee)
+    
+    class Meta:
+        unique_together = (
+            ("name", "project"),
+        )
+    
+    def give_to_permittee(self, permittee, giver=None, can_delegate=False):
+        """Give the role to a permittee. This combines the permittee's
+        permissions.
+        
+        @param permittee: object to give the role to.
+        @type permittee: object or L{Permittee} instance.
+        @keyword giver: The giver of all the permissions in the roles. If
+            not C{None}, the giver will be checked for authorization to
+            delegate the permission.
+        @type giver: Model instance or L{Permittee}
+        @keyword can_delegate: Should the permittee be able to give the
+            permissions in the role to others? Default False.
+        @type can_delegate: C{bool}.
+        """
+        
+        permittee = Permittee.objects.get_as_permittee(permittee)
+        self.permittees.add(permittee)
+        for obj_perm in self.obj_permissions.all():
+            obj_perm.give_to(
+                permittee, giver=giver, can_delegate=can_delegate)
+
+    def remove_from_permittee(self, permittee):
+        """Remove all the permissions in this role from the permittee except
+        for ones given by another role."""
+        
+        permittee = Permittee.objects.get_as_permittee(permittee)
+        self.permittees.remove(permittee)
+        
+        # Get the IDs of the other permissions.
+        other_roles = ProjectRole.objects.filter(permittees=permittee)
+        other_perms_ids = []
+        for r in other_roles:
+            other_perms_ids.extend(
+                r.obj_permissions.all().values_list("id", flat=True))
+                
+        # get the list of permissions to remove and remove all.
+        to_remove = list(self.obj_permissions.exclude(id__in=other_perms_ids))
+        PermissionOwnership.objects.filter(
+            permittee=permittee, obj_permission__in=to_remove).delete()
+    
+    def add_permission(self, obj_permission, giver=None, can_delegate=False):
+        """Add the object permission to the role and to all permittees who
+        have this role.
+        
+        @param obj_permission: The object permission to add.
+        @type obj_permission: L{ObjectPermission}.
+        @keyword giver: The giver of the permission in the roles. If
+            not C{None}, the giver will be checked for authorization to
+            delegate the permission.
+        @type giver: Model instance or L{Permittee}
+        @keyword can_delegate: Should the permittees with this role be able
+            to give the permission to others?
+        @type can_delegate: C{bool}.
+        """
+        
+        for p in self.permittees.all():
+            obj_permission.give_to(p, giver=giver, can_delegate=can_delegate)
+        self.obj_permissions.add(obj_permission)
+        
+    def remove_permission(self, obj_permission):
+        """Opposite of L{add_permission} but does not remove the permission
+        from permittees with other roles that have the permission.
+        """
+
+        self.obj_permissions.remove(obj_permission)
+
+        permittees = self.permittees.exclude(
+            projectrole__obj_permissions=obj_permission,
+        )
+        
+        PermissionOwnership.objects.filter(
+            permittee__id__in=list(
+                permittees.values_list("pk", flat=True)),
+            obj_permission=obj_permission,
+        ).delete()
+        
---
+++ src/python/expedient/clearinghouse/roles/tests.py
@@ -0,0 +1,186 @@
+'''
+Created on Aug 3, 2010
+
+@author: jnaous
+'''
+from django.test import TestCase
+from django.contrib.auth.models import User
+from expedient.clearinghouse.project.models import Project
+from expedient.common.middleware import threadlocals
+from expedient.clearinghouse.roles.models import ProjectRole
+from expedient.common.permissions.shortcuts import create_permission,\
+    has_permission
+from expedient.common.permissions.models import ObjectPermission
+from expedient.common.permissions.exceptions import PermissionCannotBeDelegated
+
+class TestModels(TestCase):
+    def setUp(self):
+        """Create a project and test permissions and permittees"""
+        self.su = User.objects.create_superuser(
+            "superuser", "su@su.com", "password")
+        self.u1 = User.objects.create_user(
+            "user1", "u@u.com", "password")
+        self.u2 = User.objects.create_user(
+            "user2", "u@u.com", "password")
+        
+        
+        self.client.login(username="superuser", password="password")
+        threadlocals.push_frame(user=self.su)
+
+        self.project = Project.objects.create(
+            name="projectX", description="blabla")
+        
+        self.client.logout()
+        threadlocals.pop_frame()
+        
+        create_permission("perm1")
+        create_permission("perm2")
+        create_permission("perm3")
+        create_permission("perm4")
+        
+        self.obj_perm1 = ObjectPermission.objects.\
+            get_or_create_for_object_or_class("perm1", self.project)[0]
+        self.obj_perm2 = ObjectPermission.objects.\
+            get_or_create_for_object_or_class("perm2", self.project)[0]
+        self.obj_perm3 = ObjectPermission.objects.\
+            get_or_create_for_object_or_class("perm3", self.project)[0]
+        self.obj_perm4 = ObjectPermission.objects.\
+            get_or_create_for_object_or_class("perm4", self.project)[0]
+        
+        self.role1 = ProjectRole.objects.create(
+            name="role1", project=self.project,
+        )
+        self.role1.obj_permissions.add(self.obj_perm1)
+        
+        self.role2 = ProjectRole.objects.create(
+            name="role2", project=self.project,
+        )
+        self.role2.obj_permissions.add(self.obj_perm2)
+
+        self.role3 = ProjectRole.objects.create(
+            name="role3", project=self.project,
+        )
+        self.role3.obj_permissions.add(self.obj_perm1)
+        self.role3.obj_permissions.add(self.obj_perm3)
+        
+    def test_give_to_permittee(self):
+        # Give the role to a user and check that she gets the permission
+        self.role1.give_to_permittee(self.u1)
+        self.assertTrue(has_permission(self.u1, self.project, "perm1"))
+        
+        # check that delegation permissions are enforced
+        self.assertRaises(
+            PermissionCannotBeDelegated,
+            self.role1.give_to_permittee,
+            self.u2,
+            giver=self.u1,
+        )
+        
+        # Give another role to the user and check that she gets the permission
+        self.role2.give_to_permittee(self.u1, can_delegate=True)
+        self.assertTrue(has_permission(self.u1, self.project, "perm2"))
+        
+        # check that delegation permissions are enforced
+        self.role2.give_to_permittee(
+            self.u2,
+            giver=self.u1,
+        )
+        self.assertTrue(has_permission(self.u1, self.project, "perm2"))
+        
+    def test_remove_from_permittee_no_conflict(self):
+        """
+        Check that a role can be removed from a permittee along with its
+        permissions when no other roles give the same permission
+        """
+        
+        self.role1.give_to_permittee(self.u1)
+        self.assertTrue(has_permission(self.u1, self.project, "perm1"))
+
+        self.role1.remove_from_permittee(self.u1)
+        self.assertFalse(has_permission(self.u1, self.project, "perm1"))
+
+    def test_remove_from_permittee_with_conflict(self):
+        """
+        Check that a role can be removed from a permittee along with its
+        non-conflicting permissions
+        """
+        
+        self.role1.give_to_permittee(self.u1)
+        self.role3.give_to_permittee(self.u1)
+        
+        self.assertTrue(has_permission(self.u1, self.project, "perm1"))
+        self.assertTrue(has_permission(self.u1, self.project, "perm3"))
+        
+        self.role1.remove_from_permittee(self.u1)
+        self.assertTrue(has_permission(self.u1, self.project, "perm1"))
+        self.assertTrue(has_permission(self.u1, self.project, "perm3"))
+
+        self.role1.give_to_permittee(self.u1)
+        self.role3.remove_from_permittee(self.u1)
+        self.assertTrue(has_permission(self.u1, self.project, "perm1"))
+        self.assertFalse(has_permission(self.u1, self.project, "perm3"))
+        
+    def test_add_permission(self):
+        """
+        Check that adding a permission to a role gives that permission to
+        all permittees with the role
+        """
+        
+        self.role1.give_to_permittee(self.u1)
+        self.role1.give_to_permittee(self.u2)
+        self.assertFalse(has_permission(self.u1, self.project, "perm2"))
+        self.assertFalse(has_permission(self.u2, self.project, "perm2"))
+        
+        self.role1.add_permission(self.obj_perm2)
+        self.assertTrue(has_permission(self.u1, self.project, "perm2"))
+        self.assertTrue(has_permission(self.u2, self.project, "perm2"))
+        
+    def test_add_permission_delegation(self):
+        """
+        Checks that add_permission works, and that delegation permissions
+        are enforced.
+        """
+        
+        self.role1.give_to_permittee(self.u1)
+        self.role1.add_permission(self.obj_perm2, can_delegate=True)
+        self.role1.add_permission(self.obj_perm3, can_delegate=True)
+        self.role2.give_to_permittee(self.u2, giver=self.u1)
+        self.role2.add_permission(self.obj_perm3, giver=self.u1)
+        self.assertTrue(has_permission(self.u2, self.project, "perm2"))
+        self.assertTrue(has_permission(self.u2, self.project, "perm3"))
+        
+        # check when delegation does not work
+        self.role2.add_permission(self.obj_perm4)
+        self.assertTrue(has_permission(self.u2, self.project, "perm4"))
+        self.assertRaises(
+            PermissionCannotBeDelegated,
+            self.role1.add_permission,
+            self.obj_perm4,
+            giver=self.u2
+        )
+        
+    def test_remove_permission(self):
+        """
+        Check that a permission can be removed from a role with and
+        without conflicts
+        """
+        self.role1.give_to_permittee(self.u1)
+        self.role1.give_to_permittee(self.u2)
+        self.role1.add_permission(self.obj_perm2)
+
+        # remove with no conflict
+        self.role1.remove_permission(self.obj_perm2)
+        self.assertFalse(has_permission(self.u1, self.project, "perm2"))
+        self.assertFalse(has_permission(self.u2, self.project, "perm2"))
+
+        # add perm and conflicting role
+        self.role2.give_to_permittee(self.u1)
+        self.role2.give_to_permittee(self.u2)
+        self.role1.add_permission(self.obj_perm2)
+        self.assertTrue(has_permission(self.u1, self.project, "perm2"))
+        self.assertTrue(has_permission(self.u2, self.project, "perm2"))
+        
+        # nothing should happen since role2 has "perm2"
+        self.role1.remove_permission(self.obj_perm2)
+        self.assertTrue(has_permission(self.u1, self.project, "perm2"))
+        self.assertTrue(has_permission(self.u2, self.project, "perm2"))
