27c653f0d6972be54308edc22a0e69e6c2e4d491
Jad Naous <jnaous@beirut.stanford.edu>
2010-08-06 17:56:49 -0700

    add tooltips for roles, add help_text, fix syntax, don't foregt to remove roles...

--- src/python/expedient/clearinghouse/project/forms.py
+++ src/python/expedient/clearinghouse/project/forms.py
@@ -8,6 +8,8 @@ from expedient.clearinghouse.project.models import Project
 from django.contrib.auth.models import User
 from expedient.clearinghouse.roles.models import ProjectRole
 from expedient.common.permissions.models import Permittee
+from django.utils.safestring import mark_safe
+from django.utils.html import conditional_escape
 
 class ProjectCreateForm(forms.ModelForm):
     """
@@ -15,6 +17,15 @@ class ProjectCreateForm(forms.ModelForm):
     """
     class Meta:
         model = Project
+
+class RoleModelChoiceField(forms.ModelMultipleChoiceField):
+    def label_from_instance(self, obj):
+        return mark_safe("<span class='val role_desc'>%s</span>" \
+            "<span class='description role_desc'>%s</span>" % (
+                conditional_escape(obj.name),
+                conditional_escape(obj.description)
+            )
+        )
 
 class AddMemberForm(forms.Form):
     """Form to add a member to a project
@@ -27,9 +38,13 @@ class AddMemberForm(forms.Form):
         able to give the roles/permissions she has to others?
     """
     
-    user = forms.ModelChoiceField(User.objects.all())
-    roles = forms.ModelMultipleChoiceField(ProjectRole.objects.all())
-    delegate = forms.BooleanField(
+    user = forms.ModelChoiceField(
+        User.objects.all(), help_text="Select the new user to add to the project.")
+    roles = RoleModelChoiceField(
+        ProjectRole.objects.all(), widget=forms.CheckboxSelectMultiple,
+        help_text="Select the roles that the user should have in this project."
+    )
+    delegate = forms.BooleanField(required=False,
         help_text="Should the new member be able to give the new permissions"
         " and roles to others?")
     
@@ -42,10 +57,10 @@ class AddMemberForm(forms.Form):
 
         super(AddMemberForm, self).__init__(*args, **kwargs)
         
-        self.user.queryset = User.objects.exclude(
+        self.fields["user"].queryset = User.objects.exclude(
             id__in=list(project.members.values_list("id", flat=True)))
         
-        self.roles.queryset = \
+        self.fields["roles"].queryset = \
             ProjectRole.objects.filter_for_can_delegate(
                 giver).filter(project=project)
         
@@ -68,8 +83,11 @@ class MemberForm(forms.Form):
     @ivar roles: C{django.forms.ModelMultipleChoiceField} to select roles
     """
     
-    roles = forms.ModelMultipleChoiceField(ProjectRole.objects.all())
-    delegate = forms.BooleanField(
+    roles = RoleModelChoiceField(
+        ProjectRole.objects.all(), widget=forms.CheckboxSelectMultiple,
+        help_text="Select the roles that the user should have in this project."
+    )
+    delegate = forms.BooleanField(required=False,
         help_text="Should the member be able to give the permissions"
         " and roles to others?")
     
@@ -92,12 +110,16 @@ class MemberForm(forms.Form):
 
         super(MemberForm, self).__init__(*args, **kwargs)
         
-        self.roles.queryset = \
+        self.fields["roles"].queryset = \
             ProjectRole.objects.filter_for_can_delegate(
                 giver).filter(project=project)
     
-        self.roles.initial = list(user.projectrole_set.filter(
-            project=project).values_list('pk', flat=True))
+        permittee = Permittee.objects.get_as_permittee(user)
+    
+        self.initial_roles = permittee.projectrole_set.filter(project=project)
+    
+        self.fields["roles"].initial = list(
+            self.initial_roles.values_list('pk', flat=True))
         
         self.user = user
         self.giver = giver
@@ -108,3 +130,7 @@ class MemberForm(forms.Form):
                 self.user,
                 giver=self.giver,
                 can_delegate=self.cleaned_data["delegate"])
+        
+        for role in self.initial_roles:
+            if role not in self.cleaned_data["roles"]:
+                role.remove_from_permittee(self.user)
