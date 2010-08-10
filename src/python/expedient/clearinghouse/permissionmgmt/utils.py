07a1a7d84da369dfc690cafe1ef74973a109ff47
Jad Naous <jnaous@gmail.com>
2010-08-10 05:04:25 -0700

    add a always_redirect_to using a callable

--- src/python/expedient/clearinghouse/permissionmgmt/utils.py
+++ src/python/expedient/clearinghouse/permissionmgmt/utils.py
@@ -7,4 +7,7 @@ from expedient.common.permissions.views import request_permission
 
 def request_permission_wrapper(*args, **kwargs):
     return request_permission(
+        always_redirect_to=\
+            lambda request: \
+                request.session.get("breadcrumbs", [(None ,"/")])[-1][1],
         template="permissionmgmt/request_permission.html")(*args, **kwargs)
