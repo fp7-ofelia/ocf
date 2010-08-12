9f22fd10aca60116ed5e08ef1980fd5322984c12
Jad Naous <jnaous@beirut.stanford.edu>
2010-08-12 02:33:06 -0700

    Robustify the postsyncdb call for permissions

--- src/python/expedient/common/permissions/management.py
+++ src/python/expedient/common/permissions/management.py
@@ -10,26 +10,6 @@ import logging
 
 logger = logging.getLogger("permissions.management")
 
-def _import_module(name, app_name):    
-    # Below is copied and modified from django's syncdb command.
-    # Import the module within each installed app, to register
-    # permissions
-    try:
-        import_module('.%s' % name, app_name)
-    except ImportError, exc:
-        # This is slightly hackish. We want to ignore ImportErrors
-        # if the "management" module itself is missing -- but we don't
-        # want to ignore the exception if the management module exists
-        # but raises an ImportError for some reason. The only way we
-        # can do this is to check the text of the exception. Note that
-        # we're a bit broad in how we check the text, because different
-        # Python implementations may not use the same text.
-        # CPython uses the text "No module named management"
-        # PyPy uses "No module named myproject.myapp.management"
-        msg = exc.args[0]
-        if not msg.startswith('No module named') or name not in msg:
-            raise
-
 # XXX: Hack!
 def post_syncdb_run(sender, **kwargs):
     """Look for permissions.run in each app and call it.
@@ -38,12 +18,23 @@ def post_syncdb_run(sender, **kwargs):
     search for all permissions modules, and try to call a C{run} function
     in them if found.
     """
-    post_syncdb_run.processed = getattr(post_syncdb_run, "processed", 0)
-    num_apps = len(settings.INSTALLED_APPS)
-    
-    # if this is the last app then install permissions for all apps
-    if post_syncdb_run.processed == num_apps - 1:
-        for app in settings.INSTALLED_APPS:
+    post_syncdb_run.queued = getattr(post_syncdb_run, "processed", [])
+    post_syncdb_run.perms_installed = getattr(
+        post_syncdb_run, "perms_installed", False)
+
+    app = sender.__name__.split(".models")[0]
+    print app
+
+    # installed!
+    if app == "expedient.common.permissions":
+        post_syncdb_run.perms_installed = True
+        
+    post_syncdb_run.queued.append(app)
+
+    # need to wait until we syncdb the permissions app
+    if post_syncdb_run.perms_installed:
+        for app in post_syncdb_run.queued:
+            logger.debug("Checking app %s for permissions" % app)
             try:
                 mod = import_module("%s.permissions" % app)
             except ImportError as exc:
@@ -55,6 +46,7 @@ def post_syncdb_run(sender, **kwargs):
                     continue
             try:
                 mod.run()
+                logger.debug("Ran permissions.run() for %s" % app)
             except AttributeError as exc:
                 msg = "%s" % exc
                 if msg == "'module' object has no attribute 'run'":
@@ -64,11 +56,6 @@ def post_syncdb_run(sender, **kwargs):
                     )
                 else:
                     raise
-        del post_syncdb_run.processed
-    else:
-        post_syncdb_run.processed += 1
-
-post_syncdb_run.waiting_for_permissions = []
-post_syncdb_run.installed_permissions = False
+        post_syncdb_run.queued = []
 
 signals.post_syncdb.connect(post_syncdb_run)
