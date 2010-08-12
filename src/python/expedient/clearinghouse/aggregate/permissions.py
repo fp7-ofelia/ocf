00c90b22aabe45d8274f010650b58d0a6736b894
Jad Naous <jnaous@beirut.stanford.edu>
2010-08-12 00:33:01 -0700

    Make permissions need a run() function to register. Solve single import test problem.

--- src/python/expedient/clearinghouse/aggregate/permissions.py
+++ src/python/expedient/clearinghouse/aggregate/permissions.py
@@ -8,23 +8,24 @@ from expedient.clearinghouse.permissionmgmt.utils import \
     request_permission_wrapper
 from expedient.clearinghouse.aggregate.views import get_can_use_permission
 
-create_permission(
-    "can_add_aggregate",
-    description="Owners of this permission can add aggregates to Expedient.",
-    view=request_permission_wrapper,
-)
-create_permission(
-    "can_edit_aggregate",
-    description=\
-        "Owners of this permission can edit or delete "
-        "the related aggregates in Expedient.",
-    view=request_permission_wrapper,
-)
-create_permission(
-    "can_use_aggregate",
-    description=\
-        "Projects, slices, and users that are owners of this permission "
-        "can use the aggregate by starting or stopping slices on it, and "
-        "by calling other methods.",
-    view=get_can_use_permission,
-)
+def run():
+    create_permission(
+        "can_add_aggregate",
+        description="Owners of this permission can add aggregates to Expedient.",
+        view=request_permission_wrapper,
+    )
+    create_permission(
+        "can_edit_aggregate",
+        description=\
+            "Owners of this permission can edit or delete "
+            "the related aggregates in Expedient.",
+        view=request_permission_wrapper,
+    )
+    create_permission(
+        "can_use_aggregate",
+        description=\
+            "Projects, slices, and users that are owners of this permission "
+            "can use the aggregate by starting or stopping slices on it, and "
+            "by calling other methods.",
+        view=get_can_use_permission,
+    )
--- src/python/expedient/common/permissions/management.py
+++ src/python/expedient/common/permissions/management.py
@@ -5,6 +5,10 @@ Created on Jun 2, 2010
 '''
 from django.utils.importlib import import_module
 from django.db.models import signals
+from django.conf import settings
+import logging
+
+logger = logging.getLogger("permissions.management")
 
 def _import_module(name, app_name):    
     # Below is copied and modified from django's syncdb command.
@@ -26,23 +30,43 @@ def _import_module(name, app_name):
         if not msg.startswith('No module named') or name not in msg:
             raise
 
+# XXX: Hack!
 def post_syncdb_run(sender, **kwargs):
-    # We need to wait until the permissions application is
-    # installed (syncdb'd)
-    if post_syncdb_run.installed_permissions:
-        _import_module("permissions", sender.__name__.split(".models")[0])
-
-    # If the permissions app is was just installed, then import the
-    # permissions of any queued application
-    elif sender.__name__.startswith("expedient.common.permissions"):
-        post_syncdb_run.installed_permissions = True
-        for mod in post_syncdb_run.waiting_for_permissions:
-            _import_module("permissions", mod.__name__.split(".models")[0])
+    """Look for permissions.run in each app and call it.
     
-    # Otherwise, queue this application until the permissions app
-    # is installed
+    This function will wait till the last installed app, and then
+    search for all permissions modules, and try to call a C{run} function
+    in them if found.
+    """
+    post_syncdb_run.processed = getattr(post_syncdb_run, "processed", 0)
+    num_apps = len(settings.INSTALLED_APPS)
+    
+    # if this is the last app then install permissions for all apps
+    if post_syncdb_run.processed == num_apps - 1:
+        for app in settings.INSTALLED_APPS:
+            try:
+                mod = import_module("%s.permissions" % app)
+            except ImportError as exc:
+                msg = exc.args[0]
+                if not msg.startswith('No module named') or\
+                "permissions" not in msg:
+                    raise
+                else:
+                    continue
+            try:
+                mod.run()
+            except AttributeError as exc:
+                msg = "%s" % exc
+                if msg == "'module' object has no attribute 'run'":
+                    logger.warn(
+                        "Function called 'run' missing from %s.permissions."\
+                        " Skipping." % app
+                    )
+                else:
+                    raise
+        del post_syncdb_run.processed
     else:
-        post_syncdb_run.waiting_for_permissions.append(sender)
+        post_syncdb_run.processed += 1
 
 post_syncdb_run.waiting_for_permissions = []
 post_syncdb_run.installed_permissions = False
