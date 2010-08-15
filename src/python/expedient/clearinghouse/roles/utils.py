d9ec609560ca0614f537e71e25eb5edbaa7055b7
Jad Naous <jnaous@beirut.stanford.edu>
2010-08-14 18:50:19 -0700

    don't use role name to get (can have multiple things with the same name.

--- src/python/expedient/clearinghouse/roles/utils.py
+++ src/python/expedient/clearinghouse/roles/utils.py
@@ -14,29 +14,26 @@ def get_users_for_role(role, can_delegate=False):
     by whether or not they were given the role explicitly. It is much slower.
     
     @param role: role whose users we are looking for.
-    @type role: instance of L{ProjectRole} or name of the role.
+    @type role: instance of L{ProjectRole}
     @keyword can_delegate: Should we look only for users who can delegate
         the role? Default is C{False}.
     @type can_delegate: C{bool}
     @return: users that can own the role
     @rtype: C{QuerySet} of C{django.contrib.auth.models.User}
     """
-    if isinstance(role, str) or isinstance(role, unicode):
-        role = ProjectRole.objects.get(name=role)
-        
     # get all the permissions in the role
     obj_perm_ids = role.obj_permissions.values_list("pk", flat=True)
     
     # Filter for all the ids
-    d = {}
     qs = Permittee.objects.all()
     for id in obj_perm_ids:
         if can_delegate:
             qs = qs.filter(
                 permissionownership__obj_permission__pk=id,
-                permissionownership__can_delegate=True)
+                permissionownership__can_delegate=True).distinct()
         else:
-            qs = qs.filter(permissionownership__obj_permission__pk=id)
+            qs = qs.filter(
+                permissionownership__obj_permission__pk=id).distinct()
         
-    user_ids = qs.values_list("object_id", flat=True)        
+    user_ids = qs.values_list("object_id", flat=True)
     return User.objects.filter(pk__in=user_ids)
