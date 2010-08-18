86f5bc6d3642b185fcdfb03cddceb89a5b21e2eb
Jad Naous <jnaous@beirut.stanford.edu>
2010-08-18 11:07:50 -0700

    Add missing permission, fix info on can_edit_user

--- src/python/expedient/clearinghouse/users/permissions.py
+++ src/python/expedient/clearinghouse/users/permissions.py
@@ -12,6 +12,13 @@ def run():
         "can_edit_user",
         description=\
             "Owners of this permission can modify information about "
-            "or delete a user.",
+            "or delete a particular user.",
+        view=request_permission_wrapper,
+    )
+    create_permission(
+        "can_manage_users",
+        description=\
+            "Owners of this permission can view/modify/delete users in the"
+            "user management page.",
         view=request_permission_wrapper,
     )
