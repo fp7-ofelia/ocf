3b652be26a48deba91ac729c22d0027726f259e3
Jad Naous <jnaous@linux-vqkp.site>
2010-08-23 00:02:50 -0700

    add required settings

--- src/python/expedient/clearinghouse/defaultsettings/required.py
+++ src/python/expedient/clearinghouse/defaultsettings/required.py
@@ -10,8 +10,9 @@ REQUIRED_SETTINGS = [
     ("django", ["SECRET_KEY"]),
     ("email", ["EMAIL_HOST", "DEFAULT_FROM_EMAIL"]),
     ("gcf", ["GCF_URN_PREFIX"]),
-    ("openflow", ["OPENFLOW_GAPI_RSC_URN_PREFIX", "OPENFLOW_GAPI_AM_URN"]),
     ("site", ["SITE_DOMAIN"]),
+    ("openflow", ["OPENFLOW_GAPI_RSC_URN_PREFIX", "OPENFLOW_GAPI_AM_URN"]),
+    ("openflowtests", ["SITE_IP_ADDR", "MININET_VMS"]),
 ]
 '''Required settings that should be overridden for each deployment.
 
