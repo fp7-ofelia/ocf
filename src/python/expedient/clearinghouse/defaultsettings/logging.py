03b292b3fd37b51278bd5f1f22984bf29445c08d
Jad Naous <jnaous@linux-vqkp.site>
2010-08-23 00:02:29 -0700

    add default logging settings

---
+++ src/python/expedient/clearinghouse/defaultsettings/logging.py
@@ -0,0 +1,15 @@
+'''Settings for loggers in Expedient.
+Created on Aug 22, 2010
+
+@author: jnaous
+'''
+
+LOGGING_LEVELS = [
+    ("permissions.management", 20),
+    ("permissions.shortcuts", 20),
+]
+'''Level settings for the various loggers in Expedient.
+
+This is a list of tuples: (logger name, logging level)
+
+'''
