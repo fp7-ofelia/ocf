b1b62a4aa4b4d91855f05b8cd4d67d0af2e528d9
Jad Naous <jnaous@beirut.stanford.edu>
2010-08-26 16:51:31 -0700

    fix log info on creating secret key

--- src/python/expedient/clearinghouse/commands/management/commands/create_secret_key.py
+++ src/python/expedient/clearinghouse/commands/management/commands/create_secret_key.py
@@ -13,9 +13,10 @@ class Command(NoArgsCommand):
     help = "Generate a new secret key and write it in secret_key.py"
 
     def handle_noargs(self, **options):
-        print "Generating new key in secret_key.py..."
-        key = ''.join([choice('abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*(-_=+)') for i in range(50)])
-        f = open(os.path.join(settings.CONF_DIR, "secret_key.py"), mode="w")
+        filepath = os.path.join(settings.CONF_DIR, "secret_key.py")
+        print "Generating new key in %s..." % filepath
+        key = ''.join([choice('abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*(-_=+)') for _ in range(50)])
+        f = open(filepath, mode="w")
         f.write("SECRET_KEY = '%s'\n" % key)
         f.close()
         print "Done."
