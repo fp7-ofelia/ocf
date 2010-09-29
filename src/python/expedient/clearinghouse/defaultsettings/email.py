a9d357b54343758fd254c43eb4dc556f23362b7b
Jad Naous <jnaous@stanford.edu>
2010-09-28 21:16:49 -0700

    clearinghouse->expedient

--- src/python/expedient/clearinghouse/defaultsettings/email.py
+++ src/python/expedient/clearinghouse/defaultsettings/email.py
@@ -13,4 +13,4 @@ EMAIL_HOST_USER = 'clearinghouse.geni@gmail.com'
 EMAIL_HOST_PASSWORD = "the password" # example
 EMAIL_PORT = 587
 DEFAULT_FROM_EMAIL = 'no-reply@geni.org'
-EMAIL_SUBJECT_PREFIX = '[GENI-Clearinghouse] '
+EMAIL_SUBJECT_PREFIX = '[GENI-Expedient] '
