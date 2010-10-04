0c6dfa4ad297562ec263a8e98bb75d836d2ab054
Jad Naous <jnaous@jadsm.site>
2010-10-03 20:54:59 -0700

    Modify FlowSpaceForm to use actual stored rules

--- src/python/expedient/ui/html/forms.py
+++ src/python/expedient/ui/html/forms.py
@@ -4,12 +4,15 @@ Created on Jun 20, 2010
 @author: jnaous
 '''
 from django import forms
-from expedient.ui.html.models import SliceFlowSpace
+from openflow.plugin.models import FlowSpaceRule
 
 class FlowSpaceForm(forms.ModelForm):
     """
     Form to edit flowspace.
     """
     class Meta:
-        model = SliceFlowSpace
-        exclude = ["slice"]
+        model = FlowSpaceRule
+
+    def __init__(self, sliver_qs, *args, **kwargs):
+        super(FlowSpaceForm, self).__init__(*args, **kwargs)
+        self.fields["slivers"].queryset = sliver_qs
