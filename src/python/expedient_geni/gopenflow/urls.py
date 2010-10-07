fb27e59ae58ee68b1c39e538efc27c4b7baffdb3
Jad Naous <jnaous@linux-vqkp.site>
2010-10-07 00:42:46 -0700

    minor

--- src/python/expedient_geni/gopenflow/urls.py
+++ src/python/expedient_geni/gopenflow/urls.py
@@ -4,7 +4,7 @@
 from django.conf.urls.defaults import patterns, url
 from expedient_geni.gopenflow.models import GCFOpenFlowAggregate
 
-urlpatterns = patterns('expedient_geni.views',
-    url(r'^aggregate/create/$', 'aggregate_create', kwargs={"agg_model": GCFOpenFlowAggregate}, name='gopenflow_aggregate_create'),
-    url(r'^aggregate/(?P<agg_id>\d+)/edit/$', 'aggregate_edit', kwargs={"agg_model": GCFOpenFlowAggregate}, name='gopenflow_aggregate_edit'),
+urlpatterns = patterns('',
+    url(r'^aggregate/create/$', 'expedient_geni.views.aggregate_create', kwargs={"agg_model": GCFOpenFlowAggregate}, name='gopenflow_aggregate_create'),
+    url(r'^aggregate/(?P<agg_id>\d+)/edit/$', 'expedient_geni.views.aggregate_edit', kwargs={"agg_model": GCFOpenFlowAggregate}, name='gopenflow_aggregate_edit'),
 )
