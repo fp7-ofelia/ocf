919f4f46ef6ee08e8c5900cee38ba7c3e42345b8
Jad Naous <jnaous@linux-vqkp.site>
2010-10-08 11:16:02 -0700

    fix bug in trying to create cert for unauthed users

--- src/python/expedient_geni/middleware.py
+++ src/python/expedient_geni/middleware.py
@@ -8,5 +8,6 @@ from expedient_geni.utils import get_or_create_user_cert
 class CreateUserGID(object):
     """Middleware to create GIDs for users who login and do not have one."""
     def process_request(self, request):
-        get_or_create_user_cert(request.user)
+        if request.user.is_authenticated():
+            get_or_create_user_cert(request.user)
 
