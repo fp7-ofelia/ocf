6efa0b7e52a446068abc30e658d6260b5e0d0c7b
Jad Naous <jnaous@linux-vqkp.site>
2010-10-12 22:43:16 -0700

    change the prefix of rspecs returned by the DummyOFAggregate

--- src/python/expedient_geni/gopenflow/tests/models.py
+++ src/python/expedient_geni/gopenflow/tests/models.py
@@ -4,7 +4,15 @@ Created on Oct 6, 2010
 @author: jnaous
 '''
 from django.db import models
-from openflow.plugin.gapi import gapi
+from django.conf import settings
+from openflow.plugin.gapi import gapi, rspec
+from geni.util.urn_util import publicid_to_urn
+
+OTHER_BASENAME ="test//test"
+OTHER_PREFIX = publicid_to_urn(
+    "IDN %s//%s"
+    % (OTHER_BASENAME, settings.OPENFLOW_GCF_BASE_SUFFIX)
+)
 
 class DummyOFAggregate(models.Model):
     adv_rspec = models.XMLField()
@@ -12,4 +20,6 @@ class DummyOFAggregate(models.Model):
     
     def snapshot_switches(self):
         self.adv_rspec = gapi.ListResources({}, None)
+        self.adv_rspec = self.adv_rspec.replace(
+            rspec.OPENFLOW_GAPI_RSC_URN_PREFIX, OTHER_PREFIX)
         self.save()
