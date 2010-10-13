689b0565fafc0b0767b5ccb83057633948fc2c39
Jad Naous <jnaous@linux-vqkp.site>
2010-10-12 22:42:46 -0700

    add new urls for new expedient_geni views

--- src/python/expedient_geni/gopenflow/urls.py
+++ src/python/expedient_geni/gopenflow/urls.py
@@ -5,6 +5,8 @@ from django.conf.urls.defaults import *
 from expedient_geni.gopenflow.models import GCFOpenFlowAggregate
 
 urlpatterns = patterns('',
-    url(r'^aggregate/create/$', 'expedient_geni.views.aggregate_create', kwargs={"agg_model": GCFOpenFlowAggregate}, name='gopenflow_aggregate_create'),
-    url(r'^aggregate/(?P<agg_id>\d+)/edit/$', 'expedient_geni.views.aggregate_edit', kwargs={"agg_model": GCFOpenFlowAggregate}, name='gopenflow_aggregate_edit'),
+    url(r'^aggregate/create/$', 'expedient_geni.gopenflow.views.aggregate_create', name='gopenflow_aggregate_create'),
+    url(r'^aggregate/(?P<agg_id>\d+)/edit/$', 'expedient_geni.gopenflow.views.aggregate_edit', name='gopenflow_aggregate_edit'),
+    url(r'^aggregate/(?P<agg_id>\d+)/links/$', 'expedient_geni.gopenflow.views.aggregate_add_links', name='gopenflow_aggregate_add_links'),
+    url(r'^aggregate/(?P<agg_id>\d+)/slice/(?P<slice_id>\d+)/add/$', 'aggregate_add_to_slice', name='gopenflow_aggregate_slice_add'),
 )
