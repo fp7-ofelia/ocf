dd6a4b218030e13c3220b306be7b25611fd3a8e0
Jad Naous <jnaous@beirut.stanford.edu>
2010-10-28 16:28:19 -0700

    bump to 0.3.4.11

--- setup.py
+++ setup.py
@@ -70,7 +70,7 @@ test.run_tests = run_tests
 
 setup(
     name="expedient",
-    version="0.3.4.10",
+    version="0.3.4.11",
     description="Modular pluggable platform to manage GENI",
     author="Jad Naous",
     author_email="jnaous@stanford.edu",
