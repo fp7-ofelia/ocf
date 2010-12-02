23f41812f9ff9066a45c831bfc2f0deb0b2a8a89
leonardo.bergesio <leonardo.bergesio@i2cat.net>
2010-12-02 17:08:59 +0100

    fist steps

---
+++ expedient/src/python/expedient/clearinghouse/localsettings.py
@@ -0,0 +1,33 @@
+CONF_DIR = '/home/leo/ofelia-git/expedient/src/python/expedient/clearinghouse'
+SRC_DIR = '/home/leo/ofelia-git/expedient/src'
+
+
+ROOT_USERNAME = "expedient"
+ROOT_PASSWORD = "expedient"
+ROOT_EMAIL    = "i2catopenflow@gmail.com"
+
+ADMINS = [
+    ("i2CAT Openflow Admin", ROOT_EMAIL),
+]
+
+MANAGERS = ADMINS
+
+EMAIL_HOST = "smtp.gmail.com"
+DEFAULT_FROM_EMAIL = "no-reply@gmail.com"
+
+GCF_BASE_NAME = "expedient//i2CAT"
+GCF_URN_PREFIX = "expedient:i2CAT"
+
+SITE_DOMAIN = "192.168.254.193"
+
+OPENFLOW_GAPI_RSC_URN_PREFIX = "urn:publicid:IDN+expedient:i2CAT:openflow"
+OPENFLOW_GAPI_AM_URN = OPENFLOW_GAPI_RSC_URN_PREFIX+"+am"
+
+SITE_IP_ADDR = "192.168.254.193"
+MININET_VMS = [
+    ("84.88.41.12", 22),
+]
+
+DATABASE_USER = "expedient"
+DATABASE_PASSWORD = "expedient"
+
