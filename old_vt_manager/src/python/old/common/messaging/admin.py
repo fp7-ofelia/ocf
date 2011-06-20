8ddfa2354f9a925f6f804f7513207557ca0c0cc2
root <root@debian.i2cat>
2011-06-20 10:57:58 +0200

    old_vt_manager deleted

--- old_vt_manager/src/python/old/common/messaging/admin.py
+++
@@ -1,9 +0,0 @@
-'''
-Created on Apr 28, 2010
-
-@author: jnaous
-'''
-import models
-from django.contrib import admin
-
-admin.site.register(models.DatedMessage)
--- old_vt_manager/src/python/vt_manager/tests/testslot.py
+++
@@ -1,6 +0,0 @@
-from vt_manager.controller.SlotAllocator import SlotAllocator
-from vt_manager.models import Slot
-m = SlotAllocator()
-a = m.acquire("p7", "am1", "s1")
-b = m.acquire("p8", "am1", "s2")
-
