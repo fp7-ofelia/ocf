b9d6a1aa7157123622d1f250e18c1d0333ab5ea9
Leonardo.Bergesio <leonardo.bergesio@i2cat.net>
2011-10-18 13:08:55 +0200

    little improvement account settings using ofreg

--- expedient/src/python/expedient/common/utils/context_processors.py
+++ expedient/src/python/expedient/common/utils/context_processors.py
@@ -2,12 +2,11 @@ def contextSettingsInTemplate(request):
 	from django.conf import settings
 	from os.path import dirname, join
 	from django.core.urlresolvers import reverse
-	from django.contrib.auth.models import AnonymousUser
 
 	vf = open(join(dirname(__file__), '../../../../../.currentVersion'),'r')
 	softwareVersion = vf.read()
 
-	if not isinstance(request.user,AnonymousUser) and request.user.password == '!':
+	if not request.user.is_anonymous() and request.user.password == '!':
 		ldapUser = True
 	else:
 		ldapUser = False
