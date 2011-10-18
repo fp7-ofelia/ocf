73a9f3b94b8981cc6a6781a948aabf830f4fd06c
Leonardo.Bergesio <leonardo.bergesio@i2cat.net>
2011-10-18 13:04:45 +0200

    little improvement account settings using ofreg

--- expedient/src/python/expedient/common/utils/context_processors.py
+++ expedient/src/python/expedient/common/utils/context_processors.py
@@ -2,12 +2,20 @@ def contextSettingsInTemplate(request):
 	from django.conf import settings
 	from os.path import dirname, join
 	from django.core.urlresolvers import reverse
+	from django.contrib.auth.models import AnonymousUser
 
 	vf = open(join(dirname(__file__), '../../../../../.currentVersion'),'r')
 	softwareVersion = vf.read()
 
+	if not isinstance(request.user,AnonymousUser) and request.user.password == '!':
+		ldapUser = True
+	else:
+		ldapUser = False
+
 	extraSettings =  {'islandName':settings.ISLAND_NAME,
 					'softwareVersion':softwareVersion,
 					'allowLocalRegistration':settings.ALLOW_LOCAL_REGISTRATION,
+					'ldapUser':ldapUser,
+					'ofregURL':settings.OFREG_URL
 					}		
 	return extraSettings 
