0390a7d969d3b0e3b2eb37785405d675ef46f1b7
Leonardo Bergesio <leonardo.bergesio@i2cat.net>
2011-11-16 12:59:34 +0100

    cleaning old folders

--- expedient/old/bin/run_flowvisor.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-source expedient-settings
-
-echo Starting the FlowVisor in a detached screen named flowvisor...
-# Run the FV
-screen -S "flowvisor" -L -dm $FLOWVISOR/scripts/flowvisor.sh $FLOWVISOR/default-config.xml
-echo To access the screen, use the command: screen -rd flowvisor
