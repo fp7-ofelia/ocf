dbda7cfaada7f05abccf10979794669724bc386e
Carlos Bermudo <carlos.bermudo@i2cat.net>
2012-04-10 12:09:53 +0200

    Fixed parseFVexception bug when exception text cannot be parsed. Added try statement.

--- expedient/src/python/expedient/clearinghouse/slice/utils.py
+++ expedient/src/python/expedient/clearinghouse/slice/utils.py
@@ -1,2 +1,5 @@
 def parseFVexception(exc):
-    return str(exc).split(':',2)[2][0:-2]
+	try:
+	    return str(exc).split(':',2)[2][0:-2]
+        except:
+            return str(exc)
--- optin_manager/src/python/openflow/optin_manager/flowspace/utils.py
+++ optin_manager/src/python/openflow/optin_manager/flowspace/utils.py
@@ -45,4 +45,7 @@ def long_to_dpid(l):
     return ":".join(m)
 
 def parseFVexception(exc):
-    return str(exc).split(':',2)[2][0:-2]
+	try:
+	    return str(exc).split(':',2)[2][0:-2]
+	except:
+	    return str(exc)
