d8da2bb076da62503a13d4f8027c5d22826224f4
Carolina Fernandez <carolina.fernandez@i2cat.net>
2012-10-03 14:42:54 +0200

    Issue #30: 'VMs containing '/' characters cannot be started' solved. Javascript validation added for the VM and Project creation

--- expedient/src/python/expedient/common/utils/validators.py
+++ expedient/src/python/expedient/common/utils/validators.py
@@ -5,7 +5,7 @@ Created on Jul 17, 2010
 '''
 import re
 from django.core.validators import RegexValidator
-from django import forms
+from django.forms import ValidationError
 
 mac_re = re.compile(r'^([\dA-Fa-f]{2}:){5}[\dA-Fa-f]{2}$')
 validate_mac_address = RegexValidator(mac_re, u'Enter a valid MAC address (in "xx:xx:xx:xx:xx:xx" format).', 'invalid')
@@ -13,7 +13,22 @@ validate_mac_address = RegexValidator(mac_re, u'Enter a valid MAC address (in "x
 ip_re = ipv4_re = re.compile(r'^(25[0-5]|2[0-4]\d|[0-1]?\d?\d)(\.(25[0-5]|2[0-4]\d|[0-1]?\d?\d)){3}(/\d?\d)?$')
 validate_ip_prefix = RegexValidator(ip_re, u'Enter a valid MAC address (in "xx:xx:xx:xx:xx:xx" format).', 'invalid')
 
+def resourceNameValidator(s):
+    pattern = re.compile("^([0-9a-zA-Z\-\_])+$")
+    if (pattern.match(s) == None):
+        raise ValidationError("Please do not use accented characters, symbols or whitespaces.")
+
+def numberValidator(s):
+    pattern = re.compile("^([0-9])+$")
+    if (pattern.match(s) == None):
+        raise ValidationError("Please use numbers only.")
+
 def asciiValidator(s):
-    for c in s:
-        if ord(c)>127 or c == '@':
-            raise forms.ValidationError("Please use only non accented characters and avoid using \'@\'.")
+    pattern = re.compile("^([0-9a-zA-Z\-\_\ ])+$")
+    if (pattern.match(s) == None):
+        raise ValidationError("Please do not use accented characters and avoid using \'@\'.")
+
+#def asciiValidator(s):
+#    for c in s:
+#        if ord(c)>127 or c == '@':
+#            raise ValidationError("Please use only non accented characters and avoid using \'@\'.")
---
+++ expedient/src/static/expedient/clearinghouse/media/default/js/validation.js
@@ -0,0 +1,91 @@
+var TEXT_RE = /^([0-9a-zA-Z\-\_\ \.])+$/;
+var RESOURCE_RE = /^([0-9a-zA-Z\-\_])+$/;
+var NUMBER_RE = /^([0-9])+$/;
+
+function doErrorlistExists(fieldID) {
+    var result = false;
+    if ($("#errorlist_" + fieldID).length > 0) {
+        result = true;
+    }
+    return result;
+}
+
+function prependErrorlist(fieldID, errorMessage) {
+    $("#" + fieldID).parent().prepend("<ul id=\"errorlist_" + fieldID + "\" class=\"error\"><li>" + errorMessage + "</li></ul>");
+}
+
+function removeErrorlist(fieldID) {
+    $("#errorlist_" + fieldID).remove();
+}
+
+function checkWithRegExp(fieldID, regExp, errorMessage) {
+    var result = false;
+    reCorrectFormat = new RegExp(regExp);
+    var field = $("#" + fieldID);
+    if (!reCorrectFormat.test(field.val())) {
+        if (!doErrorlistExists(field.attr("id"))) {
+            prependErrorlist(field.attr("id"), errorMessage);
+        }
+    } else {
+        if (doErrorlistExists(field.attr("id"))) {
+            removeErrorlist(field.attr("id"));
+        }
+        result = true;
+    }
+    return result;
+}
+
+function checkProjectName() {
+    return checkWithRegExp("id_name", TEXT_RE, "Check that the project name has ASCII characters only.");
+}
+
+function checkProjectDescription() {
+    return checkWithRegExp("id_description", TEXT_RE, "Check that the project description has ASCII characters only.");
+}
+
+function checkVirtualMachineName() {
+    return checkWithRegExp("id_form-0-name", RESOURCE_RE, "Check that the VM has ASCII characters only and no whitespaces.");
+}
+
+function checkVirtualMachineMemory() {
+    return checkWithRegExp("id_form-0-memory", NUMBER_RE, "Check that the VM size consists only of numbers.");
+}
+
+function checkDropDownSelected(fieldID) {
+    var result = false;
+    var field = $("#" + fieldID);
+    if ($("#" + fieldID).attr("selectedIndex") <= 0) {
+        if (!doErrorlistExists(field.attr("id"))) {
+            prependErrorlist(field.attr("id"), "Please select one option.");
+        }
+    } else {
+        if (doErrorlistExists(field.attr("id"))) {
+            removeErrorlist(field.attr("id"));
+        }
+        result = true;
+    }
+    return result;
+}
+
+function checkAllResultsOK(results) {
+    var result = true;
+    $.each(results, function(index, value) { result = result && value; });
+    return result;
+}
+
+function checkProjectInfo() {
+    var results = Array();
+    results[0] = checkProjectName();
+    results[1] = checkProjectDescription();
+    return checkAllResultsOK(results);
+}
+
+function checkVirtualMachineInfo() {
+    var results = Array();
+    results[0] = checkVirtualMachineName();
+    results[1] = checkVirtualMachineMemory();
+    results[2] = checkDropDownSelected("id_form-0-disc_image");
+    results[3] = checkDropDownSelected("id_form-0-hdSetupType");
+    results[4] = checkDropDownSelected("id_form-0-virtualizationSetupType");
+    return checkAllResultsOK(results);
+}
