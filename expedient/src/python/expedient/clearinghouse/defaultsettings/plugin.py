975cca09440020edad3d8fa42f383a7d3b4dc3b0
Leonardo Bergesio <leonardo.bergesio@i2cat.net>
2013-03-27 11:43:15 +0100

    plugin loading environment fixed to discover plugins models

--- expedient/src/python/expedient/clearinghouse/defaultsettings/plugin.py
+++ expedient/src/python/expedient/clearinghouse/defaultsettings/plugin.py
@@ -2,22 +2,29 @@
 Loads plugin settings in Expedient environment.
 
 @date: Feb 20, 2013
-@author: CarolinaFernandez
+@author: CarolinaFernandez, lbergesio
 """
 
-from utils import append_to_local_setting
 import os
 import sys
+config=__import__('django.conf')
+settings = config.conf.settings
+
+#print sys.path
+#print os.environ['DJANGO_SETTINGS_MODULE']
+
+#XXX lbergesio:This is ugly and prints before say sys.path and environment are right.
+#XXX Try to remove this lines.
 
 # Enable imports under this folder
-sys.path.append("/opt/ofelia/expedient/src/python/expedient/")
+sys.path.append(os.path.join(os.path.dirname(__file__), "../.."))
 # Import Django environment variables to be used inside PluginLoader class
 os.environ['DJANGO_SETTINGS_MODULE'] = "expedient.clearinghouse.settings"
 #from plugins.pluginloader import PluginLoader as PLUGIN_LOADER
 from common.utils.plugins.pluginloader import PluginLoader as PLUGIN_LOADER
 from common.utils.plugins.topologygenerator import TopologyGenerator as TOPOLOGY_GENERATOR
 
-PLUGIN_LOADER.set_plugins_path("/opt/ofelia/expedient/src/python/plugins/")
+PLUGIN_LOADER.set_plugins_path(os.path.join(os.path.dirname(__file__), "../../../plugins/"))
 PLUGIN_SETTINGS = PLUGIN_LOADER.load_settings()
 
 """
@@ -38,26 +45,15 @@ for (plugin, plugin_settings) in PLUGIN_SETTINGS.iteritems():
     for (section, section_settings) in plugin_settings.iteritems():
         for (setting, setting_value) in section_settings.iteritems():
             try:
-                # 1st: check inside locals
-                try:
-                    locals()[setting.upper()]
-                # 2nd: some vars are already set inside settings (e.g. INSTALLED_APPS)
-                # Seek these if previous failed
-                except:
-                    #exec("from settings import %s" % str(setting.upper()))
-                    tmp = __import__('settings', globals(), locals(), [setting.upper()], 0)
-                    locals()[setting.upper()] = getattr(tmp, setting.upper())
-            except:
-                # Setting does not exist in locals() => create it
-                try:
-                    locals()[setting.upper()] = []
-                except Exception as e:
-                    print "[Warning] Problem loading setting '%s'. Details: %s" % (str(setting.upper()), str(e))
-
-            if setting.upper() in locals():
-                if isinstance(setting_value, list):
-                    for value in setting_value:
-                        locals()[setting.upper()].append(value)
-                else:
-                    locals()[setting.upper()].append(setting_value)
+                conf_setting = getattr(settings, setting.upper())
+            except Exception as e:
+                setattr(settings, setting.upper(), list()) 
+                conf_setting = getattr(settings, setting.upper())
+            try:
+                if not isinstance(setting_value, list):
+                    setting_value = [setting_value]
+                conf_setting += setting_value
+            except Exception as e:
+                print "WARNING: Could not load setting : %s" % setting.upper()
+                print e
 
