08433103c8d0e15117bc736aa187394b7951b8df
Carolina Fernandez <carolina.fernandez@i2cat.net>
2013-04-09 19:10:09 +0200

    Plugin system: SampleResource plugin (sample plugin)

--- expedient/src/python/plugins/sample_resource/models/SampleResourcePlugin.py
+++ expedient/src/python/plugins/sample_resource/models/SampleResourcePlugin.py
@@ -7,7 +7,7 @@ from sample_resource.models.SampleResource import SampleResource
 # Sample Resource Plugin class
 class SampleResourcePlugin(Aggregate):
     '''
-    Sample Resource Plugin that communicates the Virtualization Aggregate Manager with Expedient
+    SampleResource Plugin that communicates the SampleResource Aggregate Manager with Expedient
     '''
     # Sample Resource Aggregate information field
     information = "An aggregate of sample resources"
@@ -16,28 +16,20 @@ class SampleResourcePlugin(Aggregate):
         app_label = 'sample_resource'
         verbose_name = "Sample Resource Aggregate"
     
-#    client = models.OneToOneField('xmlrpcServerProxy', editable = False, blank = True, null = True)
-
-
-    #def start_slice(self, slice):
-    #    super(SampleResourcePlugin, self).start_slice(slice)
-    #    try:
-    #        from sample_resource.controller.dispatchers.GUIdispatcher import startStopSlice
-    #        startStopSlice("start",slice.uuid)
-    #    except:
-    #        raise
+    client = models.OneToOneField('xmlrpcServerProxy', editable = False, blank = True, null = True)
 
     def stop_slice(self, slice):
-        super(SampleResourcePlugin, self).stop_slice(slice)
+        pass
+
+    def get_resources(self, slice_id):
         try:
-            from sample_resource.controller.dispatchers.GUIdispatcher import startStopSlice
-            startStopSlice("stop",slice.uuid)
-        except:
-            raise
+            return SampleResource.objects.filter(slice_id = slice_id, aggregate = self.pk)
+        except Exception as e:
+            return []
 
     """
-    aggregate.remove_from_project on a VT AM will get here first to check
-    that no slice inside the project contains VMs for the given aggregate
+    aggregate.remove_from_project on a SampleResource AM will get here first to check
+    that no slice inside the project contains SampleResource's for the given aggregate
     """
     def remove_from_project(self, project, next):
         # Check permission because it won't always call parent method (where permission checks)
@@ -47,7 +39,7 @@ class SampleResourcePlugin(Aggregate):
         offending_slices = []
         for resource in sample_resources:
             offending_slices.append(str(resource.SampleResource.get_slice_name()))
-        # Aggregate has VMs in slices -> stop slices and remove aggregate from there where possible
+        # Aggregate has SampleResource's in slices -> stop slices and remove aggregate from there where possible
         if offending_slices:
             for slice in project.slice_set.all():
                 try:
@@ -56,16 +48,16 @@ class SampleResourcePlugin(Aggregate):
                 except:
                     pass
             raise MultipleObjectsReturned("Please delete all Sample Resources inside aggregate '%s' before removing it from slices %s" % (self.name, str(offending_slices)))
-        # Aggregate has no VMs in slices (OK) -> delete completely from project (parent method)
+        # Aggregate has no SampleResource's in slices (OK) -> delete completely from project (parent method)
         else:
             return super(SampleResourcePlugin, self).remove_from_project(project, next)
 
     """
-    aggregate.remove_from_slice on a VT AM will get here first to check
-    that the slice does not contain VMs for the given aggregate
+    aggregate.remove_from_slice on a SampleResource AM will get here first to check
+    that the slice does not contain SampleResource's for the given aggregate
     """
     def remove_from_slice(self, slice, next):
-        # If any VM (created inside this slice) is found inside any server of the VT AM, warn
+        # If any SampleResource (created inside this slice) is found inside the SampleResource AM, warn
         if self.resource_set.filter_for_class(SampleResource).filter(sampleresource__sliceId=slice.uuid):
             raise MultipleObjectsReturned("Please delete all Sample Resources inside aggregate '%s' before removing it" % str(self.name))
         return super(SampleResourcePlugin, self).remove_from_slice(slice, next)
--- expedient/src/python/plugins/sample_resource/utils/validators.py
+++ expedient/src/python/plugins/sample_resource/utils/validators.py
@@ -7,38 +7,20 @@ Created on Jan 14, 2013
 from django.core.exceptions import ValidationError
 from django.core.validators import RegexValidator
 import re
-#from sample_resource.models.SampleResource import TEMPERATURE_SCALE_CHOICES
 
 RESOURCE_SR_RE = "^([0-9a-zA-Z\-]){1,64}$"
+TEMPERATURE_SCALE_CHOICES = (
+                        ('C','Celsius'),
+                        ('F','Fahrenheit'),
+                        ('K','Kelvin'),
+                      )
 
-#def validate_discImage(value):
-#    if value not in ["test", "default"]:
-#        raise ValidationError("Invalid input: only test image available")
-
-#def validate_hdSetupType(value):
-#    if value != "file-image":
-#        raise ValidationError("Invalid input: only File Image supported")
-
-#def validate_memory(value):
-#    if value < 128:
-#        raise ValidationError("Invalid input: memory has to be higher than 128Mb")
-
-
-def validate_temperature_scale(scale, scales):
-#    if scale not in ["celsius", "fahrenheit", "kelvin"]:
-    if scale not in scales:
+def validate_temperature_scale(scale):
+    """
+    Validates the chosen temperature scale against the set 'indices'.
+    """
+    if scale not in [ t[0] for t in TEMPERATURE_SCALE_CHOICES ]:
         raise ValidationError("Invalid scale: please choose one from the list")
-
-#validate_temperature_scale = RegexValidator(re.compile("^(celsius|fahrenheit|kelvin)$"),
-#                        u"Please choose one of the available scales.",
-#                        "invalid")
-
-
-
-
-#def validate_virtualizationSetupType(value):
-#    if value != "paravirtualization":
-#        raise ValidationError("Invalid input: only Paravirtualization supported")
 
 validate_resource_name = RegexValidator(re.compile(RESOURCE_SR_RE),
                         u"Please do not use accented characters, symbols, underscores or whitespaces.",
