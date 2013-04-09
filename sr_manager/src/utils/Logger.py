374c61d4372ee2664478dcc08916758ea4626139
Carolina Fernandez <carolina.fernandez@i2cat.net>
2013-04-09 19:33:24 +0200

    Plugin system: SampleResource AM (sample AM)

---
+++ sr_manager/src/utils/Logger.py
@@ -0,0 +1,34 @@
+import logging
+import threading 
+
+
+#Get log level
+from settings.settingsLoader import LOG_LEVEL 
+
+#Configure logger
+log_level= ""
+if LOG_LEVEL == "INFO":
+	log_level = logging.INFO
+elif LOG_LEVEL == "WARNING":
+	log_level = logging.WARNING
+elif LOG_LEVEL == "ERROR":
+	log_level = logging.ERROR
+elif LOG_LEVEL == "CRITICAL":
+	log_level = logging.CRITICAL
+else:
+	log_level = logging.DEBUG
+
+threadId="main"
+try:
+	threadId = threading.current_thread().OXAId
+except:
+	pass
+
+logging.basicConfig(format='%(asctime)s [%(threadName)s>%(filename)s:%(lineno)d] %(levelname)s: %(message)s', datefmt='%d/%m/%Y %I:%M:%S %p', level=log_level)
+
+class Logger():
+	@staticmethod
+	def getLogger():
+		#Simple wrapper. Ensures logging is always correctly configured (logging.basicConfig is executed)
+		return logging.getLogger()
+
