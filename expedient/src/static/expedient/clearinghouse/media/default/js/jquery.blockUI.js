7cfa9d2612494fe47c30008798e87e4fc4932721
Carolina Fernandez <carolina.fernandez@i2cat.net>
2013-04-15 13:16:26 +0200

    Issue #124: 'Warning OpenFlow message when starting/updating/stopping a slice' (though further improvements needed)

--- expedient/src/static/expedient/clearinghouse/media/default/js/jquery.blockUI.js
+++ expedient/src/static/expedient/clearinghouse/media/default/js/jquery.blockUI.js
@@ -70,7 +70,7 @@ $.blockUI.version = 2.39; // 2nd generation blocking at no extra cost!
 // override these in your code to change the default behavior and style
 $.blockUI.defaults = {
 	// message displayed when blocking (use null for no message)
-	message:  '<h1>Please wait...</h1>',
+	message:  '<h1 style="color: #fff !important;">Please wait...</h1>',
 
 	title: null,	  // title string; only used when theme == true
 	draggable: true,  // only used when theme == true (requires jquery-ui.js to be loaded)
