88bdfa1cb9d7c8a88f2d53fa2d1b9e90f4b42150
Leonardo Bergesio <leonardo.bergesio@i2cat.net>
2013-04-25 13:00:30 +0200

    List Resources Complete

--- optin_manager/src/python/openflow/optin_manager/sfa/OFShell.py
+++ optin_manager/src/python/openflow/optin_manager/sfa/OFShell.py
@@ -1,13 +1,10 @@
 import threading
 import time
-#from openflow.optin_manager import xmlrpc_server
-from openflow.optin_manager.xmlrpc_server.models import CallBackServerProxy, FVServerProxy
-
 import re
-
 from openflow.optin_manager.sfa.openflow_utils.CreateOFSliver import CreateOFSliver
 from openflow.optin_manager.sfa.openflow_utils.rspec3_to_expedient import get_fs_from_group 
 from openflow.optin_manager.opts.models import Experiment, ExperimentFLowSpace
+from openflow.optin_manager.xmlrpc_server.models import CallBackServerProxy, FVServerProxy
 
 class OFShell:
 
@@ -20,29 +17,21 @@ class OFShell:
     		try:
         		switches = flow_visor.get_switches()
     		except Exception as e:
-        		#import traceback
-        		#traceback.print_exc()
-        		#raise Exception(parseFVexception(e))
-			#XXX: this is what we have...
+                        #XXX:Test-Only
 			switches = [('00:00:00:00:00:00:00:09', {'nPorts': '3', 'portList': '65534,1,2', 'portNames': 'dp0(65534),s9-eth1(1),s9-eth2(2)', 'remote': '/10.216.12.5:6633-->/10.216.126.8:57545', 'dpid': '00:00:00:00:00:00:00:09'}), ('00:00:00:00:00:00:00:0a', {'nPorts': '4', 'portList': '3,2,65534,1', 'portNames': 's10-eth3(3),s10-eth2(2),dp1(65534),s10-eth1(1)', 'remote': '/10.216.12.5:6633-->/10.216.126.8:57546', 'dpid': '00:00:00:00:00:00:00:0a'}), ('00:00:00:00:00:00:00:0d', {'nPorts': '4', 'portList': '3,2,65534,1', 'portNames': 's13-eth3(3),s13-eth2(2),dp4(65534),s13-eth1(1)', 'remote': '/10.216.12.5:6633-->/10.216.126.8:57549', 'dpid': '00:00:00:00:00:00:00:0d'}), ('00:00:00:00:00:00:00:0e', {'nPorts': '4', 'portList': '3,2,65534,1', 'portNames': 's14-eth3(3),s14-eth2(2),dp5(65534),s14-eth1(1)', 'remote': '/10.216.12.5:6633-->/10.216.126.8:57550', 'dpid': '00:00:00:00:00:00:00:0e'}), ('00:00:00:00:00:00:00:0f', {'nPorts': '4', 'portList': '3,2,65534,1', 'portNames': 's15-eth3(3),s15-eth2(2),dp6(65534),s15-eth1(1)', 'remote': '/10.216.12.5:6633-->/10.216.126.8:57551', 'dpid': '00:00:00:00:00:00:00:0f'}), ('00:00:00:00:00:00:00:0b', {'nPorts': '4', 'portList': '3,2,65534,1', 'portNames': 's11-eth3(3),s11-eth2(2),dp2(65534),s11-eth1(1)', 'remote': '/10.216.12.5:6633-->/10.216.126.8:57552', 'dpid': '00:00:00:00:00:00:00:0b'}), ('00:00:00:00:00:00:00:0c', {'nPorts': '4', 'portList': '3,2,65534,1', 'portNames': 's12-eth3(3),s12-eth2(2),dp3(65534),s12-eth1(1)', 'remote': '/10.216.12.5:6633-->/10.216.126.8:57553', 'dpid': '00:00:00:00:00:00:00:0c'})] 
-		print '---------------------SWITCHES,SWITCHES',switches
-                print '---------------------USED SWS', used_switches
     		for switch in switches:
-                        print switch[0]
                         if len(used_switches)>0:
                              	if not switch[0] in used_switches:
-                                    print '--------------------------continue'
                                     continue
-                        print '-------------por aqui no continues'
-                        print switch
+                        if int(switch[1]['nPorts']) == 0:
+                            raise Exception("The switch with dpid:%s has a connection problem. The OCF Island Manager is already informed, please try again later" % str(switch[0]))
+                            #TODO: Send Mail to the Island Manager Here.
 			port_list = switch[1]['portNames'].split(',')
-			print port_list
 			ports = list()
 			for port in port_list:
 				match = re.match(r'[\s]*(.*)\((.*)\)', port)
 				ports.append({'port_name':match.group(1), 'port_num':match.group(2)})
-			complete_list.append({'dpid':switch[0], 'ports':ports})
-			
+
 		return complete_list
 
 	@staticmethod
@@ -51,10 +40,7 @@ class OFShell:
                 try:
                         links = flow_visor.get_links()
 		except Exception as e:
-			#import traceback
-                        #traceback.print_exc()
-                        #raise Exception(parseFVexception(e))
-                        #XXX: this is what we have...
+                        #XXX:Test-Only
 			links = [('00:00:00:00:00:00:00:0d', '3', '00:00:00:00:00:00:00:09', '2', {}), ('00:00:00:00:00:00:00:0a', '2', '00:00:00:00:00:00:00:0c', '3', {}), ('00:00:00:00:00:00:00:0f', '3', '00:00:00:00:00:00:00:0d', '2', {}), ('00:00:00:00:00:00:00:0a', '3', '00:00:00:00:00:00:00:09', '1', {}), ('00:00:00:00:00:00:00:0a', '1', '00:00:00:00:00:00:00:0b', '3', {}), ('00:00:00:00:00:00:00:0d', '2', '00:00:00:00:00:00:00:0f', '3', {}), ('00:00:00:00:00:00:00:0d', '1', '00:00:00:00:00:00:00:0e', '3', {}), ('00:00:00:00:00:00:00:09', '1', '00:00:00:00:00:00:00:0a', '3', {}), ('00:00:00:00:00:00:00:09', '2', '00:00:00:00:00:00:00:0d', '3', {}), ('00:00:00:00:00:00:00:0c', '3', '00:00:00:00:00:00:00:0a', '2', {}), ('00:00:00:00:00:00:00:0b', '3', '00:00:00:00:00:00:00:0a', '1', {}), ('00:00:00:00:00:00:00:0e', '3', '00:00:00:00:00:00:00:0d', '1', {})]
 		link_list = list()
 		for link in links:
@@ -76,9 +62,7 @@ class OFShell:
                         for expfs in expfss:
                             if not expfs.dpid in nodes:
                                 nodes.append(expfs.dpid)
-                    nodes=['00:00:00:00:00:00:00:09']#XXX: Hack for now 
                     switches = self.get_switches(flow_visor, nodes)
-                    print '-----------------------------------------------------SWITCHES', switches
                     return {'switches':switches, 'links':[]}
 
 	def GetSlice(self,slicename,authority):
