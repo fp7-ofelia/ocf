7375939862d5629955431cb39bba4fb78cd0db3c
Carolina Fernandez <carolina.fernandez@i2cat.net>
2013-07-22 10:45:21 +0200

    Standardized the location of the message center

--- expedient/src/views.py
+++ expedient/src/views.py
@@ -6,13 +6,13 @@ Created on Jun 19, 2010
 
 from django.views.generic.simple import direct_to_template
 from django.core.urlresolvers import reverse
-from common.permissions.shortcuts import has_permission 
-from django.contrib.auth.models import User
+#from common.permissions.shortcuts import has_permission 
+#from django.contrib.auth.models import User
 
 def home(request):
     return direct_to_template(
         request,
-        template='index.html',
+        template="index.html",
         extra_context={
             "breadcrumbs": (
                 ("Home", reverse("home")),
