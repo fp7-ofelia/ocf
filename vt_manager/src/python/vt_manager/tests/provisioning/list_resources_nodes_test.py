c578d827b3f846deec53aeea53a8c962ea0a2ff9
Oscar Moya <oscar.moya@i2cat.net>
2013-08-21 11:30:38 +0200

    New method to list resources with nodes available in the north communication interface

---
+++ vt_manager/src/python/vt_manager/tests/provisioning/list_resources_nodes_test.py
@@ -0,0 +1,39 @@
+import os
+import sys
+import uuid, random 
+from os.path import dirname, join
+
+PYTHON_DIR = join(dirname(__file__), '/opt/ofelia/vt_manager/src/python/')
+
+# This is needed because wsgi disallows using stdout
+sys.stdout = sys.stderr
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'vt_manager.settings'
+
+sys.path.insert(0,PYTHON_DIR)
+
+
+from vt_manager.models.VTServer import VTServer
+from vt_manager.communication.utils.XmlHelper import *
+from vt_manager.communication.XmlRpcClient import XmlRpcClient
+from vt_manager.communication.southCommInterface import *
+import xmlrpclib
+
+url = 'https://expedient:expedient@192.168.254.170:8445/xmlrpc/plugin'
+server = xmlrpclib.Server(url)
+print server.ListResourcesAndNodes()
+
+#AGENT DEVUELVE ONGOING
+#import time
+#time.sleep(10)
+#response = XmlHelper.parseXmlString(xmlFileToString('failresponse.xml'))
+#response.response.provisioning.action[0].id=actionUUID
+#response.response.provisioning.action[0].status="ONGOING"
+#sendAsync(XmlHelper.craftXmlClass(response))
+#
+#
+##AGENT DEVUELVE FAIL
+#time.sleep(10)
+#response = XmlHelper.parseXmlString(xmlFileToString('failresponse.xml'))
+#response.response.provisioning.action[0].id=actionUUID
+#sendAsync(XmlHelper.craftXmlClass(response))
