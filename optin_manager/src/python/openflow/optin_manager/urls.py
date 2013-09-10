1aff5d852824f6c459f854073eb2c3b4d64833bd
Carolina Fernandez <carolina.fernandez@i2cat.net>
2013-09-10 19:11:09 +0200

    Opt-in: solved issue with XMLRPC urls (that disappeared at merge)

--- optin_manager/src/python/openflow/optin_manager/urls.py
+++ optin_manager/src/python/openflow/optin_manager/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls.defaults import *
 from django.contrib import admin
 from django.conf import settings
-from expedient.common.rpc4django.utils import rpc_url
+from common.rpc4django.utils import rpc_url
 
 from openflow.common.utils.OptinThemeManager import OptinThemeManager
 OptinThemeManager.initialize()
