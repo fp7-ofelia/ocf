26ed045cebf7ce96fadc7de85294e5be5b2e6d6b
Carolina Fernandez <carolina.fernandez@i2cat.net>
2013-11-08 17:22:22 +0100

    Forms improved, buttons standardized, title nav text increased

--- expedient/src/python/expedient/clearinghouse/defaultsettings/messaging.py
+++ expedient/src/python/expedient/clearinghouse/defaultsettings/messaging.py
@@ -9,5 +9,5 @@ Created on Aug 19, 2010
 NUM_LATEST_MSGS = 10
 '''Number of messages to display on the dashboard.'''
  
-NUM_CONTEXT_MSGS = 5
+NUM_CONTEXT_MSGS = 3
 '''Number of messages to display at the top of pages.'''
