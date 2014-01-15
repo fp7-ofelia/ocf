6e8fbbee68dd90b518b0f5ba45d6421e8361d8a3
Sergio Vidiella <sergio.vidiella@i2cat.net>
2014-01-15 12:42:39 +0100

    Method for Tables from Models generation added

--- AMsoil/src/vendor/virtrm/utils/base.py
+++ AMsoil/src/vendor/virtrm/utils/base.py
@@ -3,10 +3,18 @@ from flask.ext.sqlalchemy import SQLAlchemy
 import amsoil.core.pluginmanager as pm
 
 config = pm.getService("config")
+# Create a new SQLAlchemy instance
+db = SQLAlchemy()
+# Create  the Flask app
 app = Flask(__name__)
+# Add the database path
 app.config['SQLALCHEMY_DATABASE_URI'] = "%s://%s:%s@%s/%s" % (
                                                 config.get("virtrm.DATABASE_ENGINE"), config.get("virtrm.DATABASE_USER"),
                                                 config.get("virtrm.DATABASE_PASSWORD"), config.get("virtrm.DATABASE_HOST"),
                                                 config.get("virtrm.DATABASE_NAME"),
                                                 )
-db = SQLAlchemy(app)
+db.init_app(app)
+
+def set_up():
+    with app.app_context()
+        db.create_all()
