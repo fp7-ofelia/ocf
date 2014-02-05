0e9e0f7e3857a0de54c79a1815aaa203b123a4c8
Carolina Fernandez <carolina.fernandez@i2cat.net>
2014-02-05 18:23:53 +0100

    Fixing installation of FlowVisor

--- deploy/dependencies/flowvisor.sh
+++ deploy/dependencies/flowvisor.sh
@@ -10,7 +10,8 @@
 
 FLOWVISOR_RELEASE="1.4.0-1"
 
-if [[ $(dpkg -l | grep flowvisor | cut -d " " -f3) != $FLOWVISOR_RELEASE ]]; then
+# If dpkg shows entry with $FLOWVISOR_RELEASE on it, do not install
+if [[ $(dpkg -l | grep flowvisor) =~ $FLOWVISOR_RELEASE ]]; then
   echo "FlowVisor $FLOWVISOR_RELEASE already installed. Skipping..."
   exit 1
 fi
