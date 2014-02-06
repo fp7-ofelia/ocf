be059d997bc0c3cc6d4e757a2e6fef7e6ee5ab50
Carolina Fernandez <carolina.fernandez@i2cat.net>
2014-02-06 18:04:21 +0100

    Fixing upgrade call to common dependencies

--- deploy/upgrade.py
+++ deploy/upgrade.py
@@ -10,6 +10,7 @@ Upgrades OCF modules.
 ocf_modules_upgrade = utils.invoke_splash_screen("upgrade", utils.get_installed_modules())
 
 if ocf_modules_upgrade:
-    utils.install_dependencies(ocf_modules_upgrade)
+    # Perform installation of common files
+    utils.install_dependencies(utils.common_path, ocf_modules_upgrade)
     # Perform upgrade of already installed modules
     utils.upgrade_modules(ocf_modules_upgrade)
