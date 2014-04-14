3326e025b3f2ecf9c0d662fc2d645ff7cba975c1
Carolina Fernandez <carolina.fernandez@i2cat.net>
2014-04-14 18:56:59 +0200

    Fixing problem when stopping slices

--- optin_manager/src/python/openflow/optin_manager/xmlrpc_server/ch_api.py
+++ optin_manager/src/python/openflow/optin_manager/xmlrpc_server/ch_api.py
@@ -19,6 +19,7 @@ from django.core.mail import send_mail
 from openflow.optin_manager.flowspace.utils import int_to_mac, int_to_dotted_ip
 from django.contrib.sites.models import Site
 from openflow.optin_manager.opts.autofsgranter import auto_fs_granter
+import uuid
 
 @decorator
 def check_fv_set(func, *arg, **kwargs):
@@ -256,7 +257,6 @@ def create_slice(slice_id, project_name, project_description,
             raise Exception
     except:
         # New slice naming style (for FlowVisor >= 1.0) -> No legacy slice
-        import uuid
         try:
             uuid.UUID('{%s}' % str(slice_id))
             is_legacy_slice = False 
