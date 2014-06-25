b906a864275583d1530d91b5cf51282dd536e4fe
Sergio Vidiella <sergio.vidiella@i2cat.net>
2014-06-25 12:23:02 +0200

    Update ProvisioningResponseDispatcher to stable version

--- AMsoil/src/virtrm/vendor/virtrm/controller/dispatchers/provisioning/response.py
+++ AMsoil/src/virtrm/vendor/virtrm/controller/dispatchers/provisioning/response.py
@@ -28,21 +28,33 @@ class ProvisioningResponseDispatcher():
             # then SUCCESS or FAILED actions are finished
             if action_model.get_status() is Action.QUEUED_STATUS or Action.ONGOING_STATUS:
                 logging.debug("The incoming response has id: %s and NEW status: %s",action_model.uuid,action_model.status)
-                action_model.status = action.status
-                action_model.description = action.description
-                action_model.save()
+                action_model.set_status(action.status)
+                action_model.set_description(action.description)
                 # Complete information required for the Plugin: action type and VM
                 ActionController.complete_action_rspec(action, action_model)
                 # XXX: Implement this method or some other doing this job
                 vm = VTDriver.get_vm_by_uuid(action_model.get_object_uuid())
                 controller=VTDriver.get_driver(vm.get_server().get_virtualization_technology())
                 # Update VM after obtaining a response code
-                fail_on_create_vm = ProvisioningResponseDispatcher.__update_vm_after_response(action_model, vm)
+                failed_on_create = False
+                if action_model.get_status() == Action.SUCCESS_STATUS:
+                    ProvisioningResponseDispatcher.__update_vm_after_SUCCESS(action_model, vm)
+                elif action_model.get_status() == Action.ONGOING_STATUS:
+                    ProvisioningResponseDispatcher.update_vm_after_ONGOING(action_model, vm)
+                elif action_model.get_status() == Action.FAILED_STATUS:
+                    # Return result of fail_on_create_vm
+                    return ProvisioningResponseDispatcher.__update_vm_after_FAILED(action_model, vm)
+                else:
+                    vm.set_state(VirtualMachine.UNKNOWN_STATE)
+
+= ProvisioningResponseDispatcher.__update_vm_after_response(action_model, vm)
                 try:
                     logging.debug("Sending response to Plugin in sendAsync")
                     XmlRpcClient.callRPCMethod(vm.getCallBackURL(),"sendAsync",XmlHelper.craft_xml_class(rspec))
-                    if fail_on_create_vm:
+                    if failed_on_create:
                         controller.delete_vm(vm)
+                        # Keep actions table up-to-date after each deletion
+                        action_model.destroy()
                 except Exception as e:
                     logging.error("Could not connect to Plugin in sendAsync\n%s",e)
                     return
@@ -57,59 +69,44 @@ class ProvisioningResponseDispatcher():
                     return
         
     @staticmethod
-    def __update_vm_after_response(action_model, vm):
-        """
-        Dispatches to one or another method to update VM
-        status after a response is obtained.
-        """
-        action_status = action_model.get_status()
-        action_type = action_type
-        if action_status == Action.SUCCESS_STATUS:
-            ProvisioningResponseDispatcher.__update_vm_after_SUCCESS(action_type, vm)
-        elif action_status == Action.ONGOING_STATUS:
-            ProvisioningResponseDispatcher.update_vm_after_ONGOING(action_type, vm)
-        elif action_status == Action.FAILED_STATUS:
-            # Return result of fail_on_create_vm
-            return ProvisioningResponseDispatcher.__update_vm_after_FAILED(action_type, vm)
-        else:
-            vm.set_state(VirtualMachine.UNKNOWN_STATE)
-        
-    @staticmethod
-    def __update_vm_after_SUCCESS(action_type, vm):
-        if action_type == Action.PROVISIONING_VM_CREATE_TYPE:
+    def __update_vm_after_SUCCESS(action_model, vm):
+        if action_model.get_type() == Action.PROVISIONING_VM_CREATE_TYPE:
             vm.set_state(VirtualMachine.CREATED_STATE)
-        elif action_type is Action.PROVISIONING_VM_START_TYPE or Action.PROVISIONING_VM_REBOOT_TYPE:
+        elif action_model.get_type() is Action.PROVISIONING_VM_START_TYPE or Action.PROVISIONING_VM_REBOOT_TYPE:
             vm.set_state(VirtualMachine.RUNNING_STATE)
-        elif action_type == Action.PROVISIONING_VM_STOP_TYPE:
+        elif action_model.get_type() == Action.PROVISIONING_VM_STOP_TYPE:
             vm.set_state(VirtualMachine.STOPPED_STATE)
-        elif action_type == Action.PROVISIONING_VM_DELETE_TYPE:
+        elif action_model.get_type() == Action.PROVISIONING_VM_DELETE_TYPE:
             controller = VTDriver.get_driver(vm.vtserver.get_virt_tech())
             controller.delete_vm(vm)
+            # Keep actions table up-to-date after each deletion
+            action_model.destroy()
         
     @staticmethod
     def update_vm_after_ONGOING(action_model, vm):
-        if action_type == Action.PROVISIONING_VM_CREATE_TYPE:
+        if action_model.get_type() == Action.PROVISIONING_VM_CREATE_TYPE:
             vm.set_state(VirtualMachine.CREATING_STATE)
-        elif action_type == Action.PROVISIONING_VM_START_TYPE:
+        elif action_model.get_type() == Action.PROVISIONING_VM_START_TYPE:
             vm.set_state(VirtualMachine.STARTING_STATE)
-        elif action_type == Action.PROVISIONING_VM_STOP_TYPE:
+        elif action_model.get_type() == Action.PROVISIONING_VM_STOP_TYPE:
             vm.set_state(VirtualMachine.STOPPING_STATE)
-        elif action_type == Action.PROVISIONING_VM_DELETE_TYPE:
+        elif action_model.get_type() == Action.PROVISIONING_VM_DELETE_TYPE:
             vm.set_state(VirtualMachine.DELETING_STATE)
-        elif action_type == Action.PROVISIONING_VM_REBOOT_TYPE:
+        elif action_model.get_type() == Action.PROVISIONING_VM_REBOOT_TYPE:
             vm.set_state(VirtualMachine.REBOOTING_STATE)
         
     @staticmethod
     def __update_vm_after_FAILED(action_model, vm):
-        if  action_type == Action.PROVISIONING_VM_START_TYPE:
+        if  action_model.get_type() == Action.PROVISIONING_VM_START_TYPE:
             vm.set_state(VirtualMachine.STOPPED_STATE)
-        elif action_type == Action.PROVISIONING_VM_STOP_TYPE:
+        elif action_model.get_type() == Action.PROVISIONING_VM_STOP_TYPE:
             vm.set_state(VirtualMachine.RUNNING_STATE)
-        elif action_type == Action.PROVISIONING_VM_REBOOT_TYPE:
+        elif action_model.get_type() == Action.PROVISIONING_VM_REBOOT_TYPE:
             vm.set_state(VirtualMachine.STOPPED_STATE)
-        elif action_type == Action.PROVISIONING_VM_CREATE_TYPE:
+        elif action_model.get_type() == Action.PROVISIONING_VM_CREATE_TYPE:
             # VM deleted after sending response to the Plugin because callBackUrl is required
-            # Return fail_on_create_vm = True
-            return True
+            # Return failed_on_create = True
+            failed_on_create = True
+            return failed_on_create
         else:
             vm.set_state(VirtualMachine.FAILED_STATE)
