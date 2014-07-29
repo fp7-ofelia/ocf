c0f9a8cf37a2a8633f0d3eae7344b6b2452b86d9
Carolina Fernandez <carolina.fernandez@i2cat.net>
2014-07-29 18:27:43 +0200

    handler: added first tests for Delete method

--- ambase/src/test/geni/v3/handler/deletetest.py
+++ ambase/src/test/geni/v3/handler/deletetest.py
@@ -6,34 +6,37 @@ from src.test.utils.mockdelegate import MockDelegate
 from src.geni.exceptions.manager import GENIExceptionManager
 
 
-class DeleteTest(unittest.TestCase):
-    ''' Testing Very Basic behaviour to see 
+class AllocateTest(unittest.TestCase):
+    """ Testing very basic behaviour to see 
         whether the Handler is able to respond
         with error_results or success_results  
-    '''
+    """
     
     def setUp(self):
         self.handler = GeniV3Handler()
-        self.handler.set_credential_manager(MockCredentialManager)
-        self.handler.set_rspec_manager(MockRSpecManager)
-        self.handler.set_delegate(MockDelegate)
-        self.handler.set_geni_exception_manager(GENIExceptionManager) #is too simple to mock it
-        
+        self.handler.set_credential_manager(MockCredentialManager())
+        self.handler.set_rspec_manager(MockRSpecManager())
+        self.handler.set_delegate(MockDelegate())
+        self.handler.set_geni_exception_manager(GENIExceptionManager()) # Too simple to mock
+    
     def tearDown(self):
         self.handler = None
-        
+    
     def test_should_delete(self):
-        value = self.handler.Delete(None,None,{})
+        value = self.handler.Delete(None, None, {})
         self.assertEquals(GENIExceptionManager.SUCCESS, value.get('code').get('geni_code'))
     
+    def test_should_fail_when_invalid_credentials(self):
+        self.handler.set_credential_manager(MockCredentialManager(False))
+        value = self.handler.Delete(None, None, {})
+        self.assertEquals(GENIExceptionManager.FORBIDDEN, value.get('code').get('geni_code'))
+    
     def test_should_fail_when_deleting_wrong_sliver(self):
-        pass
+        self.handler.set_delegate(MockDelegate(False))
+        value = self.handler.Delete(None, None, {})
+        self.assertEquals(GENIExceptionManager.ERROR, value.get('code').get('geni_code'))
     
     def test_should_send_correctly_formatted_output(self):
-        pass
-    
-    def test_should_fail_when_invalid_credentials(self):
-        pass
-    
-    
-        
+        self.handler.set_delegate(MockDelegate(True))
+        value = self.handler.Delete(None, None, {})
+        self.assertEquals(GENIExceptionManager.SUCCESS, value.get('code').get('geni_code'))
