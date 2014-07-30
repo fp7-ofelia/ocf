78b71c9844f6bd8a953446bf160e5e70de1c4b6a
Carolina Fernandez <carolina.fernandez@i2cat.net>
2014-07-30 17:40:31 +0200

    GENIv3 delegate: added schema. Must be filled

---
+++ ambase/src/geni/v3/delegate/delegate.py
@@ -0,0 +1,55 @@
+from abc import ABCMeta
+from abc import abstractmethod
+
+# XXX Isn't this supposed to be used this way?
+from src.abstract.classes.delegatebase import DelegateBase
+
+class GeniV3Delegate(DelegateBase):
+    
+    """
+    Handle internal exceptions to pass to the handler
+    """
+    
+    def __init__(self):
+        self.__resource_manager = None
+    
+    def get_version(self):
+        return
+    
+    def list_resources(self, geni_available=False):
+        return
+    
+    def describe(self, urns=dict()):
+        return
+
+    def reserve(self, slice_urn, am,expiration):
+        """
+        Allocate slivers
+        """
+        # If there is no value, use empty string
+        #slice_urn = slice_urn or ""
+        return
+    
+    def create(self, urns=list()):
+        """
+        Provision slivers
+        """
+        return
+    
+    def delete(self, urns=list()):
+        """
+        Delete slivers
+        """
+        return
+    
+    def perform_operational_action(self, urns=list(), action=None, geni_besteffort=True):
+        return
+    
+    def status(self, urns=list()):
+        return
+        
+    def renew(self, urns=list(), expiration_time=None):
+        return
+        
+    def shut_down(self, urns=list()):
+        return
