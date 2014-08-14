7262ce9aecc65ab25c91419cc41e38afc32635a8
Carolina Fernandez <carolina.fernandez@i2cat.net>
2014-08-14 13:04:03 +0200

    Refactoring RSpecManagerBase

--- federation/ambase/src/abstract/classes/rspecmanager.py
+++ federation/ambase/src/abstract/classes/rspecmanager.py
@@ -2,17 +2,32 @@ from abc import ABCMeta
 from abc import abstractmethod
 
 class RSpecManager:
+    """
+    Abstract class, to be implemented in a new class.
+    """
     __metaclass__ = ABCMeta
     
     @abstractmethod
-    def advertise_resources(self, resources):
+    def compose_advertisement(self, resources):
+        """
+        Returns the advertisement RSpec using a set of 
+        resources as input.
+        """
         return
     
     @abstractmethod
-    def manifest_slivers(self, slivers):
+    def compose_manifest(self, slivers):
+        """
+        Returns the manifest RSpec using a set of slivers 
+        (group of resources) as input.
+        """
         return
     
     @abstractmethod
-    def parse_request(self, rspec):
+    def parse_request(self, request_rspec):
+        """
+        Translates the request RSpec into a set of
+        resources.
+        """
         return
 
