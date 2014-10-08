959ef838aa07c60f6ee9a938d4f3712a13bcd17c
Oscar Moya <oscar.moya@i2cat.net>
2014-10-08 10:27:27 +0200

    Openflow RSpec manager finalised

--- federation/rspecs/src/geni/v3/openflow/manager.py
+++ federation/rspecs/src/geni/v3/openflow/manager.py
@@ -1,16 +1,19 @@
 from ambase.src.abstract.classes.rspecmanagerbase import RSpecManagerBase
+from rspecs.src.geni.v3.openflow.foamlibcrafter import FOAMLibCrafter
+from rspecs.src.geni.v3.openflow.foamlibparser import FOAMLibParser
 
 class OpenFlowRSpecManager(RSpecManagerBase):
     
     def __init__(self):
-        pass
+        self.__crafter = FOAMLibCrafter()
+        self.__parser = FOAMLibParser()
     
     def compose_advertisement(self, resources):
-        return ""
+        return self.__crafter.get_advertisement(resources)
     
     def compose_manifest(self, slivers):
-        return ""
+        return self.__crafter.get_manifest(slivers)
     
-    def parse_request(self):
-        return None
+    def parse_request(self,rspec):
+        return self.__parser.parse_request(rspec)
     
