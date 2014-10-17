8828a5834088cccc8a46c83e0320ecf2bd6f7323
Oscar Moya <oscar.moya@i2cat.net>
2014-10-17 15:29:40 +0200

    adapting correct structure of getVersion

--- federation/ambase/src/geni/v3/delegate/delegate.py
+++ federation/ambase/src/geni/v3/delegate/delegate.py
@@ -26,14 +26,20 @@ class GeniV3Delegate(DelegateBase):
                        schema=self.__config.AD_RSPEC_SCHEMA,
                        namespace=self.__config.AD_RSPEC_NAMESPACE,
                        extensions=self.__config.AD_RSPEC_EXTENSIONS)]
+        
+        code_dict = dict(geni_code = self.__config.AM_CODE_VERSION,
+                         am_type=self.__config.AM_TYPE,
+                         am_code=self.__config.AM_CODE_VERSION)
+        
         api_versions = dict()
         api_versions[str(self.__config.GENI_API_VERSION)] = self.__config.AM_URL
         credential_types = [dict(geni_type = self.__config.CREDENTIAL_TYPE,
                                  geni_version = self.__config.GENI_API_VERSION)]
         versions = dict(geni_api= self.__config.GENI_API_VERSION,
                         geni_api_versions=api_versions,
-                        geni_am_type=self.__config.AM_TYPE,
-                        geni_am_code=self.__config.AM_CODE_VERSION,
+                        #geni_am_type=self.__config.AM_TYPE,
+                        #geni_am_code=self.__config.AM_CODE_VERSION,
+                        code=code_dict,
                         geni_request_rspec_versions=reqver,
                         geni_ad_rspec_versions=adver,
                         geni_credential_types=credential_types)
