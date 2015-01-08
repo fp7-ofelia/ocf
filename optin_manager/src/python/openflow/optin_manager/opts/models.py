2f3e6b442b817ed5e4f6b8816f92de24ec3d7333
Carolina Fernandez <carolina.fernandez@i2cat.net>
2015-01-08 12:54:54 +0100

    Improve format style in flowspace shown in Opt-in

--- optin_manager/src/python/openflow/optin_manager/opts/models.py
+++ optin_manager/src/python/openflow/optin_manager/opts/models.py
@@ -81,9 +81,9 @@ class ExperimentFLowSpace(FlowSpace):
         
         pn = ""
         if self.port_number_s == self.port_number_e:
-            pn = "port number: %d"%self.port_number_s
+            pn = ", port number: %d" % self.port_number_s
         elif self.port_number_s > 0 or self.port_number_e < 0xFFFF:
-            pn = "port number: %d-%d"%(self.port_number_s,self.port_number_e)
+            pn = ", port number: %d-%d" % (self.port_number_s,self.port_number_e)
             
         dir = ""
         if self.direction==2:
@@ -93,7 +93,7 @@ class ExperimentFLowSpace(FlowSpace):
         elif self.direction==0:
             dir = ", direction: egress"
             
-        return "dpid: %s, FS: %s%s%s"%(self.dpid,fs_desc,pn,dir)   
+        return "dpid: %s, FS: %s%s%s" % (self.dpid, fs_desc, pn, dir)   
     
 '''
 UserOpts, OptsFlowSpace stores information about each opt-in
